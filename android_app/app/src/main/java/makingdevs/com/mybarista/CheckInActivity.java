dbb4bdaf1695702dedf21e1f6b2b26d2d1b07c8c
says33 <says.rodriguez@gmail.com>
2016-05-17 09:35:46 -0500

    #2 agregando la implementacionde retrofit y los eventos del boton de la view

--- android_app/app/src/main/java/makingdevs/com/mybarista/CheckInActivity.java
+++ android_app/app/src/main/java/makingdevs/com/mybarista/CheckInActivity.java
@@ -6,19 +6,30 @@ import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Spinner;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+
+import makingdevs.com.mybarista.model.Contributor;
+import makingdevs.com.mybarista.service.GitHubService;
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class CheckInActivity extends AppCompatActivity {
 
     private static final String TAG = "CheckInActivity";
+    public static final String API_URL = "https://api.github.com";
     private Spinner methodCoffe;
     private Button checkInButton;
 
     private void loadMethodsCoffe(){
-        methodCoffe = (Spinner) findViewById(R.id.spinner1);
+        methodCoffe = (Spinner) findViewById(R.id.methodSpinner);
         List<String> list = new ArrayList<String>();
         list.add("Expresso");
         list.add("Americano");
@@ -41,9 +52,23 @@ public class CheckInActivity extends AppCompatActivity {
         checkInButton.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                Log.d(TAG, "huesos");
+                getFormCheckIn();
+                saveCheckIn();
             }
         });
+    }
+
+    private void getFormCheckIn(){
+        final EditText originEditText = (EditText) findViewById(R.id.originField);
+        final EditText priceEditText = (EditText) findViewById(R.id.priceField);
+        final EditText noteEditText = (EditText) findViewById(R.id.noteField);
+        String origin = originEditText.getText().toString();
+        String price = priceEditText.getText().toString();
+        String note = noteEditText.getText().toString();
+
+        final Spinner methodFieldSprinner = (Spinner) findViewById(R.id.methodSpinner);
+        String method = methodFieldSprinner.getSelectedItem().toString();
+
     }
 
     @Override
@@ -54,4 +79,36 @@ public class CheckInActivity extends AppCompatActivity {
         loadMethodsCoffe();
         initListenerCheckIn();
     }
+
+    private void saveCheckIn() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl(API_URL)
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+
+        // Create an instance of our GitHub API interface.
+        GitHubService github = retrofit.create(GitHubService.class);
+
+        // Create a call instance for looking up Retrofit contributors.
+        Call<List<Contributor>> call = github.contributors("square", "retrofit");
+
+
+        call.enqueue(new Callback<List<Contributor>>() {
+            @Override
+            public void onResponse(Call<List<Contributor>> call, Response<List<Contributor>> response) {
+                Log.d(TAG,response.body().toString());
+            }
+
+            @Override
+            public void onFailure(Call<List<Contributor>> call, Throwable t) {
+                Log.d(TAG,"fallo",t);
+            }
+        });
+
+
+    }
 }
+
+
+
+
