c369c76797156e9e66a6df47b82bf346745daf8a
says33 <says.rodriguez@gmail.com>
2016-05-18 08:30:02 -0500

    #3 Se agrega el modelo para checkin

---
+++ android_app/app/src/main/groovy/makingdevs/com/mybarista/model/Checkin.groovy
@@ -0,0 +1,12 @@
+package  makingdevs.com.mybarista.model
+
+class Checkin {
+
+    String created_at
+    String id
+    String method
+    String note
+    String origin
+    String price
+    String updated_at
+}
