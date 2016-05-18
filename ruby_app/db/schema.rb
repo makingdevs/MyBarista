5ba42dda4e6af184b93b032e9f111472a58f1c02
says33 <says.rodriguez@gmail.com>
2016-05-18 14:45:06 -0500

    #8 se agrega el chambio en el scheme de user

--- ruby_app/db/schema.rb
+++ ruby_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160516162447) do
+ActiveRecord::Schema.define(version: 20160518165720) do
 
   create_table "checkins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "method",                             null: false
@@ -20,6 +20,16 @@ ActiveRecord::Schema.define(version: 20160516162447) do
     t.string   "note"
     t.datetime "created_at",                         null: false
     t.datetime "updated_at",                         null: false
+    t.integer  "user_id"
+    t.index ["user_id"], name: "index_checkins_on_user_id", using: :btree
   end
 
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.string   "username"
+    t.string   "token"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_foreign_key "checkins", "users"
 end
