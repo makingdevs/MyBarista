f1db61285cfa3da4355f5afeb7841099b8d25732
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-18 11:04:39 -0500

    #6 agregando el limpiado del formulario y mostrando mensajes de success y fail

--- android_app/app/src/main/groovy/makingdevs/com/mybarista/CheckInActivity.groovy
+++ android_app/app/src/main/groovy/makingdevs/com/mybarista/CheckInActivity.groovy
@@ -1,5 +1,6 @@
-package makingdevs.com.mybarista;
+package makingdevs.com.mybarista
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -7,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Spinner
+import android.widget.Toast
 import groovy.transform.CompileStatic
 import makingdevs.com.mybarista.model.Checkin
 import makingdevs.com.mybarista.service.ApiService
@@ -19,8 +21,13 @@ import retrofit2.converter.gson.GsonConverterFactory;
 @CompileStatic
 public class CheckInActivity extends AppCompatActivity {
 
-    private static final String TAG = "CheckInActivity";
-    private Button checkInButton;
+    private static final String TAG = "CheckInActivity"
+    private EditText originEditText
+    private EditText priceEditText
+    private EditText noteEditText
+    private Spinner methodFieldSprinner
+    private Button checkInButton
+    private static Context contextView
 
     private final Retrofit retrofit = new Retrofit.Builder()
             .addConverterFactory(GsonConverterFactory.create())
@@ -40,23 +47,16 @@ public class CheckInActivity extends AppCompatActivity {
     }
 
     private Map<String,String> getFormCheckIn(){
-        final EditText originEditText = (EditText) findViewById(R.id.originField);
-        final EditText priceEditText = (EditText) findViewById(R.id.priceField);
-        final EditText noteEditText = (EditText) findViewById(R.id.noteField);
-        String origin = originEditText.getText().toString();
-        String price = priceEditText.getText().toString();
-        String note = noteEditText.getText().toString();
-        final Spinner methodFieldSprinner = (Spinner) findViewById(R.id.methodSpinner);
-        String method = methodFieldSprinner.getSelectedItem().toString();
+        originEditText = (EditText) findViewById(R.id.originField)
+        priceEditText = (EditText) findViewById(R.id.priceField)
+        noteEditText = (EditText) findViewById(R.id.noteField)
+        methodFieldSprinner = (Spinner) findViewById(R.id.methodSpinner)
+        String origin = originEditText.getText().toString()
+        String price = priceEditText.getText().toString()
+        String note = noteEditText.getText().toString()
+        String method = methodFieldSprinner.getSelectedItem().toString()
         Map<String,String> parameters = new HashMap<String, String>()
         parameters << ["method":method,"note":note,"origin":origin,"price":price?.toString()]
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_check_in);
-        initListenerCheckIn();
     }
 
     private void saveCheckIn(Map<String,String> parameters) {
@@ -65,12 +65,35 @@ public class CheckInActivity extends AppCompatActivity {
         def callback = [
                 onResponse :{Call<Checkin> call, Response<Checkin> response ->
                     Log.d(TAG,response.dump().toString())
-                                Log.d(TAG, response.message())
-                            },
-                onFailure : {Call<Checkin> call, Throwable t -> Log.d(TAG, "el error",t) }
+                    if (response.code() == 201){
+                        Toast.makeText(contextView,R.string.toastCheckinSuccess,Toast.LENGTH_SHORT).show();
+                        cleanForm()
+                    }else {
+                        Toast.makeText(contextView,R.string.toastCheckinFail,Toast.LENGTH_SHORT).show();
+                    }
+                },
+                onFailure : {Call<Checkin> call, Throwable t ->
+                                Toast.makeText(contextView,R.string.toastCheckinFail,Toast.LENGTH_SHORT).show();
+                            }
         ]
         model.enqueue(callback as Callback<Checkin>)
     }
+
+    private void cleanForm(){
+        originEditText.setText("")
+        priceEditText.setText("")
+        noteEditText.setText("")
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_check_in)
+        //TODO esto no sabemos si se puede hacer de una mejor manera
+        contextView = this
+        initListenerCheckIn()
+    }
+
 }
 
 
