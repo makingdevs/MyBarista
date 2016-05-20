06932707f44a9d219023d4099325efb501e266aa
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-20 16:31:08 -0500

    #15 se  eliminan los comment

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/WithFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/WithFragment.groovy
@@ -2,9 +2,6 @@ package com.makingdevs.mybarista.common
 
 import android.support.v4.app.Fragment
 
-/**
- * Created by makingdevs on 20/05/16.
- */
 trait WithFragment {
     abstract Fragment createFragment()
 }
