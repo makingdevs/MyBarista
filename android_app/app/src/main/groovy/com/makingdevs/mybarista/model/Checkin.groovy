4dc0d1c457e6675746f10879ff4672ae156ff39e
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-20 10:56:51 -0500

    #14 se crea el modelo del checkin para persistencia y poblacion de el objeto

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/Checkin.groovy
@@ -0,0 +1,13 @@
+package  com.makingdevs.mybarista.model
+
+class Checkin {
+
+    String created_at
+    String id
+    String method
+    String note
+    String origin
+    String price
+    String updated_at
+    String user_id
+}
