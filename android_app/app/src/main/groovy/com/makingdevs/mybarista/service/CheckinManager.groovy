f3e2f36a9b66cd1b229a66ebfdead36cd986c562
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 12:29:16 -0500

    #17 se cambia el tipop de elementos del parametro de mapa a checkinCommand

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
@@ -1,6 +1,8 @@
 package com.makingdevs.mybarista.service
 
+import com.makingdevs.mybarista.model.command.CheckinCommand
+
 interface CheckinManager {
-    void save(Map params, Closure onSuccess, Closure onError)
+    void save(CheckinCommand checkin, Closure onSuccess, Closure onError)
     void list(Map params, Closure onSuccess, Closure onError)
 }
