ee66af4936544e47f1297e34f5c0034577a2fc93
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 12:50:12 -0500

    #17 agregando metodo estatico para inicializar el intnt con el context de la anterior activity

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/CheckInActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/CheckInActivity.groovy
@@ -1,5 +1,7 @@
 package com.makingdevs.mybarista.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.FormCheckinFragment
@@ -7,6 +9,11 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 public class CheckinActivity extends SingleFragmentActivity {
+
+    static Intent newIntentWithContext(Context context){
+        Intent intent = new Intent(context, CheckinActivity)
+        intent
+    }
 
     @Override
     Fragment createFragment() {
