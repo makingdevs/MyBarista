48e1f37599fdf198ea8426b90e9c557758b01ddf
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 12:51:13 -0500

    #17 agregando el resume para la actualizacion de la lista y agregando el boton para llevarnos a la vista de checkin

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ListBrewFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ListBrewFragment.groovy
@@ -1,7 +1,9 @@
 package com.makingdevs.mybarista.ui.fragment
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.Nullable
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.support.v7.widget.LinearLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -13,6 +15,8 @@ import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.model.Checkin
 import com.makingdevs.mybarista.service.CheckinManager
 import com.makingdevs.mybarista.service.CheckingManagerImpl
+import com.makingdevs.mybarista.ui.activity.CheckinActivity
+import com.makingdevs.mybarista.ui.activity.ListBrewActivity
 import com.makingdevs.mybarista.ui.adapter.BrewAdapter
 import groovy.transform.CompileStatic
 import retrofit2.Call
@@ -23,8 +27,9 @@ public class ListBrewFragment extends Fragment {
 
     RecyclerView mListBrew
     BrewAdapter mBrewAdapter
-    CheckinManager mCheckinManager = CheckingManagerImpl.instance
+    FloatingActionButton mButtonGoChekin
 
+    CheckinManager mCheckinManager = CheckingManagerImpl.instance
 
     ListBrewFragment(){}
 
@@ -34,8 +39,24 @@ public class ListBrewFragment extends Fragment {
         View root = inflater.inflate(R.layout.fragment_list_brew,container, false)
         mListBrew = (RecyclerView) root.findViewById(R.id.list_brews)
         mListBrew.setLayoutManager(new LinearLayoutManager(getActivity()))
+
+        mButtonGoChekin= (FloatingActionButton) root.findViewById(R.id.button_go_chekin);
+        mButtonGoChekin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = CheckinActivity.newIntentWithContext(getContext())
+                startActivity(intent)
+            }
+        });
+
         updateUI()
         root
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume()
+        updateUI()
     }
 
     void updateUI() {
