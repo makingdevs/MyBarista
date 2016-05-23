b9169b539c732bd07fa0bb88c0508b46b4cef8d7
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 12:28:06 -0500

    #17 se ajjusta la recepcion de parametros en la interfaces

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CheckinRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CheckinRestOperations.groovy
@@ -1,7 +1,9 @@
 package com.makingdevs.mybarista.network
 
 import com.makingdevs.mybarista.model.Checkin
+import com.makingdevs.mybarista.model.command.CheckinCommand
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.Field
 import retrofit2.http.FormUrlEncoded
 import retrofit2.http.GET
@@ -14,12 +16,7 @@ public interface CheckinRestOperations {
     @GET("checkins")
     Call<List<Checkin>> getCheckins(@QueryMap Map<String, String> options)
 
-    @FormUrlEncoded
     @POST("checkins")
-    Call<Checkin> createCheckinForm(@Field("method") String method,
-                                    @Field("note") String note,
-                                    @Field("origin") String origin,
-                                    @Field("price") String price,
-                                    @Field("username") String username);
+    Call<Checkin> createCheckinForm(@Body CheckinCommand checkin)
 
 }
