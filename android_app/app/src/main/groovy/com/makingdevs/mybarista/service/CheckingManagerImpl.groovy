708e961df7db56ef5180a3380ff4e07c1b6d8e62
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 09:41:57 -0500

    #16 implementando el metodo de lista para lkos checkins

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckingManagerImpl.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckingManagerImpl.groovy
@@ -14,4 +14,12 @@ class CheckingManagerImpl implements CheckinManager {
             restOperations.createCheckinForm(params.method,params.note, params.origin, params.price,"neodevelop")
         }
     }
+
+    @Override
+    void list(Map params, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations, onSuccess, onError){ CheckinRestOperations restOperations ->
+            restOperations.getCheckins([username:"neodevelop"])
+
+        }
+    }
 }
