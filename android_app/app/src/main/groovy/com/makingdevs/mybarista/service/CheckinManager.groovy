25fb8a02844a53374a7204cb3b3175f8482aaa18
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-23 09:41:06 -0500

    #16 se agrega metodo a la interface de checkin manager para la lista de checkins

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
@@ -2,4 +2,5 @@ package com.makingdevs.mybarista.service
 
 interface CheckinManager {
     void save(Map params, Closure onSuccess, Closure onError)
+    void list(Map params, Closure onSuccess, Closure onError)
 }
