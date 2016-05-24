298d0d2585a4d7392814414422e8e52f3435b252
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-24 09:24:57 -0500

    #18 se define metodo en la interface de chekinManager para el show de un checkin

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
@@ -5,4 +5,5 @@ import com.makingdevs.mybarista.model.command.CheckinCommand
 interface CheckinManager {
     void save(CheckinCommand checkin, Closure onSuccess, Closure onError)
     void list(Map params, Closure onSuccess, Closure onError)
+    void show(String id, Closure onSuccess, Closure onError)
 }
