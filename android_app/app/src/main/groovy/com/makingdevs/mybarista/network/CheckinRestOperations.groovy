0fb06abe7124bcf1ad84b2384b8b161eeb632de2
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-25 14:14:18 -0500

    #25 Se optimizan imports de dependencias

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CheckinRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CheckinRestOperations.groovy
@@ -3,13 +3,7 @@ package com.makingdevs.mybarista.network
 import com.makingdevs.mybarista.model.Checkin
 import com.makingdevs.mybarista.model.command.CheckinCommand
 import retrofit2.Call
-import retrofit2.http.Body
-import retrofit2.http.Field
-import retrofit2.http.FormUrlEncoded
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Path
-import retrofit2.http.QueryMap
+import retrofit2.http.*
 
 public interface CheckinRestOperations {
 
@@ -21,5 +15,7 @@ public interface CheckinRestOperations {
 
     @GET("checkins/{id}")
     Call<Checkin> getCheckin(@Path("id") String id)
+
+
 
 }
