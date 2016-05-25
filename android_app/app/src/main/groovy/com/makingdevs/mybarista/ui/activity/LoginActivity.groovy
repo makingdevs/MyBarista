6e460d398133630065b764bd8ffba66865147be5
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-25 14:54:07 -0500

    #26 Se agrega metodo para mostrar este activity

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/LoginActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/LoginActivity.groovy
@@ -1,5 +1,7 @@
 package com.makingdevs.mybarista.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.LoginFragment
@@ -7,6 +9,11 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 class LoginActivity extends SingleFragmentActivity{
+
+    static Intent newIntentWithContext(Context context){
+        Intent intent = new Intent(context, LoginActivity)
+        intent
+    }
 
     @Override
     Fragment createFragment() {
