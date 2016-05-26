9923940339715ac55d9fe7608316a58088c0d95a
says33 <says.rodriguez@gmail.com>
2016-05-26 10:50:09 -0500

    #27 se agrega la referencia de los cambios en la tabla users

--- ruby_app/db/schema.rb
+++ ruby_app/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160525165223) do
+ActiveRecord::Schema.define(version: 20160526153204) do
 
   create_table "checkins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.integer  "method",                             null: false
@@ -27,9 +27,9 @@ ActiveRecord::Schema.define(version: 20160525165223) do
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.string   "token"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string   "password"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.string   "password_digest"
     t.string   "name"
     t.string   "lastName"
   end
