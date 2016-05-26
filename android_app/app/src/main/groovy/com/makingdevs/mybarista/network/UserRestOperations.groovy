f1517e52341090f57ab5785f5859dddf46494f53
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-26 11:25:04 -0500

    #28 agregando metodo en las operaciones de userRest para el login

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/UserRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/UserRestOperations.groovy
@@ -4,9 +4,16 @@ import com.makingdevs.mybarista.model.RegistrationCommand
 import com.makingdevs.mybarista.model.User
 import retrofit2.Call
 import retrofit2.http.Body
+import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.QueryMap
 
 public interface UserRestOperations {
+
     @POST("users")
     Call<User> registrationUser(@Body RegistrationCommand registration)
+
+    @GET("login/user")
+    Call<User> loginUser(@QueryMap Map<String, String> login)
+
 }
