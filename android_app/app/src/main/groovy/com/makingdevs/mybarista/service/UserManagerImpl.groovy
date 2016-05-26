ab07ba607deb7d2a11982224fd3df1a26ca6b3a7
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-26 11:35:57 -0500

    #28 agregando implementacion del login en el servicio de user manager

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManagerImpl.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManagerImpl.groovy
@@ -1,6 +1,7 @@
 package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.RegistrationCommand
+import com.makingdevs.mybarista.model.command.LoginCommand
 import com.makingdevs.mybarista.network.UserRestOperations
 import com.makingdevs.mybarista.network.impl.RetrofitTemplate
 
@@ -14,6 +15,13 @@ class UserManagerImpl implements UserManager {
     void save(RegistrationCommand registrationCommand, Closure onSuccess, Closure onError) {
         RetrofitTemplate.instance.withRetrofitUser(operations, onSuccess, onError) { UserRestOperations restOperations ->
             restOperations.registrationUser(registrationCommand)
+        }
+    }
+
+    @Override
+    void login(LoginCommand loginCommand, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofitUser(operations, onSuccess, onError) { UserRestOperations restOperations ->
+            restOperations.loginUser([username:loginCommand.username,password:loginCommand.password])
         }
     }
 }
