d15dd0cccf486471b5007d5081e03d5734114022
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-26 11:35:13 -0500

    #28 se agrega metodo a la interface para el login de un user

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManager.groovy
@@ -1,7 +1,11 @@
 package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.RegistrationCommand
+import com.makingdevs.mybarista.model.command.LoginCommand
 
 interface UserManager {
+
     void save(RegistrationCommand registrationCommand, Closure onSuccess, Closure onError)
+    void login(LoginCommand loginCommand, Closure onSuccess, Closure onError)
+
 }
