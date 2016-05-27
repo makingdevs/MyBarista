a96b071ddadc8f1ae8c218a57dd336bde22f1b44
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-27 11:58:18 -0500

    #31 se agrega validacion al command esto para el registro

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/RegistrationCommand.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/RegistrationCommand.groovy
@@ -6,4 +6,9 @@ class RegistrationCommand{
     String password
     String confirmPassword
 
+    Boolean validateCommand(){
+        def pattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}/
+        this.username ==~ pattern && this.password == this.confirmPassword && this.username != this.password
+    }
+
 }
