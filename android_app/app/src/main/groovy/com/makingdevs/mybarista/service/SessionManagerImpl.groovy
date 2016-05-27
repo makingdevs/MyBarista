5584805ae49829f6496847388c8431640a3da02c
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-27 10:46:02 -0500

    #31 creando la implementacion para la session con shared preferences

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/SessionManagerImpl.groovy
@@ -0,0 +1,23 @@
+package com.makingdevs.mybarista.service
+
+import android.content.Context
+import android.content.SharedPreferences
+import com.makingdevs.mybarista.model.User
+
+@Singleton
+class SessionManagerImpl implements SessionManager{
+
+    void setUserSession(User user, Context context) {
+        SharedPreferences.Editor session = context.getSharedPreferences("UserSession", Context.MODE_PRIVATE).edit()
+        session.putString("username",user.username)
+        session.putString("token",user.token)
+        session.commit()
+    }
+
+    Map getUserSession(Context context) {
+        SharedPreferences session = context.getSharedPreferences("UserSession",Context.MODE_PRIVATE)
+        String username = session.getString("username",null);
+        String token = session.getString("token",null);
+        [username:username,token:token]
+    }
+}
