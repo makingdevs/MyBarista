93009ea274a945f81a0d224b9976567a7d96e51b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-27 08:33:12 -0500

    #29 agregando la logica del intent en el activity de lista

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
@@ -1,5 +1,7 @@
 package com.makingdevs.mybarista.ui.activity
 
+import android.content.Context
+import android.content.Intent
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.ListBrewFragment
@@ -7,6 +9,11 @@ import groovy.transform.CompileStatic
 
 @CompileStatic
 public class ListBrewActivity extends SingleFragmentActivity {
+
+    static Intent newIntentWithContext(Context context){
+        Intent intent = new Intent(context, ListBrewActivity)
+        intent
+    }
 
     @Override
     Fragment createFragment() {
