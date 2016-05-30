816dc33da96807d2788f5688b1f551972eb19047
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-30 10:36:44 -0500

    #35 Se agrega el metodo para mostrar el toolbar

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
@@ -2,6 +2,8 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.ListBrewFragment
@@ -18,5 +20,11 @@ public class ListBrewActivity extends SingleFragmentActivity {
     @Override
     Fragment createFragment() {
         new ListBrewFragment()
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        createMenu()
     }
 }
