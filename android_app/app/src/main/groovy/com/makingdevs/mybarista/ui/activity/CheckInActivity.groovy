d9a65b086b6d9202183baad10ce586908622d820
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-30 12:35:06 -0500

    #38 Se agrega metodo para mostrar menuç

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/CheckInActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/CheckInActivity.groovy
@@ -2,6 +2,8 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.FormCheckinFragment
@@ -20,4 +22,9 @@ public class CheckinActivity extends SingleFragmentActivity {
         new FormCheckinFragment()
     }
 
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        createMenu()
+    }
 }
