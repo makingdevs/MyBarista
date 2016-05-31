28aeea8aa3ef674d12f35ec83e4dda9c6014dea0
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-31 15:34:57 -0500

    #42 se agrega la implemntacion de get user y update user en el administrador de usuario

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManagerImpl.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManagerImpl.groovy
@@ -2,6 +2,7 @@ package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.RegistrationCommand
 import com.makingdevs.mybarista.model.command.LoginCommand
+import com.makingdevs.mybarista.model.command.UpdateUserCommand
 import com.makingdevs.mybarista.network.UserRestOperations
 import com.makingdevs.mybarista.network.impl.RetrofitTemplate
 
@@ -22,6 +23,20 @@ class UserManagerImpl implements UserManager {
     void login(LoginCommand loginCommand, Closure onSuccess, Closure onError) {
         RetrofitTemplate.instance.withRetrofitUser(operations, onSuccess, onError) { UserRestOperations restOperations ->
             restOperations.loginUser([username:loginCommand.username,password:loginCommand.password])
+        }
+    }
+
+    @Override
+    void update(UpdateUserCommand updateUserCommand, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofitUser(operations, onSuccess, onError) { UserRestOperations restOperations ->
+            restOperations.updateUser(updateUserCommand.id,updateUserCommand)
+        }
+    }
+
+    @Override
+    void getUser(String id, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofitUser(operations, onSuccess, onError) { UserRestOperations restOperations ->
+            restOperations.getUser(id)
         }
     }
 }
