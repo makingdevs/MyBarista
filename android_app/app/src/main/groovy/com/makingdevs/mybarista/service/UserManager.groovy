5eb6dbb5741fe158ce60f0b8943d96fa25b4e77b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-31 15:31:55 -0500

    #42 se agrega definicion de metodos en la interface de user manager errorz en el ultimo commit

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/UserManager.groovy
@@ -2,10 +2,13 @@ package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.RegistrationCommand
 import com.makingdevs.mybarista.model.command.LoginCommand
+import com.makingdevs.mybarista.model.command.UpdateUserCommand
 
 interface UserManager {
 
     void save(RegistrationCommand registrationCommand, Closure onSuccess, Closure onError)
     void login(LoginCommand loginCommand, Closure onSuccess, Closure onError)
+    void update(UpdateUserCommand updateUserCommand, Closure onSuccess, Closure onError)
+    void getUser(String id, Closure onSuccess, Closure onError)
 
 }
