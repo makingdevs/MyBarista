9782d027ed0ea719f21ea05b444a0c836a6e5a15
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-05-31 15:33:49 -0500

    #42 se agrega dos metodos en la interafce de retrofit

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/UserRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/UserRestOperations.groovy
@@ -2,10 +2,14 @@ package com.makingdevs.mybarista.network
 
 import com.makingdevs.mybarista.model.RegistrationCommand
 import com.makingdevs.mybarista.model.User
+import com.makingdevs.mybarista.model.UserProfile
+import com.makingdevs.mybarista.model.command.UpdateUserCommand
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.PUT
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 public interface UserRestOperations {
@@ -15,5 +19,11 @@ public interface UserRestOperations {
 
     @GET("login/user")
     Call<User> loginUser(@QueryMap Map<String, String> login)
+
+    @GET("users/{id}")
+    Call<UserProfile> getUser(@Path("id") String id)
+
+    @PUT("users/{id}")
+    Call<UserProfile> updateUser(@Path("id") String id, @Body UpdateUserCommand updateUserCommand)
 
 }
