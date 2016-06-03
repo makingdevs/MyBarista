35b12e217eab3e0448082699331241da371ef773
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-03 18:37:46 -0500

    #50 cambiando el tipo de dato

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/model/Checkin.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/Checkin.groovy
@@ -2,12 +2,13 @@ package  com.makingdevs.mybarista.model
 
 class Checkin {
 
-    String created_at
+    Date created_at
     String id
     String method
     String note
     String origin
     String price
-    String updated_at
+
+    Date updated_at
     String user_id
 }
