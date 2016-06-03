c86eb9606030db87c3a0dfd91649025cdc6dd829
says <says.rodriguez@gmail.com>
2016-06-03 15:14:03 -0500

    #48 se crea el activity para la crecion de el circulo del sabor

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/CircleFlavorActivity.groovy
@@ -0,0 +1,23 @@
+package com.makingdevs.mybarista.ui.activity
+
+import android.content.Context
+import android.content.Intent
+import android.support.v4.app.Fragment
+import com.makingdevs.mybarista.common.SingleFragmentActivity
+import com.makingdevs.mybarista.ui.fragment.CircleFlavorFragment
+import groovy.transform.CompileStatic
+
+@CompileStatic
+public class CircleFlavorActivity extends SingleFragmentActivity {
+
+    static Intent newIntentWithContext(Context context){
+        Intent intent = new Intent(context, CircleFlavorActivity)
+        intent
+    }
+
+    @Override
+    Fragment createFragment() {
+        new CircleFlavorFragment()
+    }
+
+}
