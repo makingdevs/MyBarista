40c5fd1fb38a36efd010355977c4a9f7916257d4
says <says.rodriguez@gmail.com>
2016-06-03 16:13:11 -0500

    #51 se agrega el modelo de circle flovor para la respuesta del circulo del sabor

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/CircleFlavor.groovy
@@ -0,0 +1,15 @@
+package com.makingdevs.mybarista.model
+
+class CircleFlavor {
+
+    String dulzura
+    String acidez
+    String floral
+    String especiado
+    String salado
+    String frutosRojos
+    String chocolate
+    String caramelo
+    String cuerpo
+    String limpieza
+}
