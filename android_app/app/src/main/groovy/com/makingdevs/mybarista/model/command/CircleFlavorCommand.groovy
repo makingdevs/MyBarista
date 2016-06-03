5b82ff7b0d35e5669236a08132d7b7b7c9d2d49a
says <says.rodriguez@gmail.com>
2016-06-03 16:13:43 -0500

    #51 se crea el command para enviar los parametros del circulo del sabor a la base de datos

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/CircleFlavorCommand.groovy
@@ -0,0 +1,15 @@
+package  com.makingdevs.mybarista.model.command
+
+class CircleFlavorCommand {
+
+    String dulzura
+    String acidez
+    String floral
+    String especiado
+    String salado
+    String frutosRojos
+    String chocolate
+    String caramelo
+    String cuerpo
+    String limpieza
+}
