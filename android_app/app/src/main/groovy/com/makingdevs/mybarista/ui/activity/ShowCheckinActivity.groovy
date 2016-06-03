d6fba3cd9544569413547549c45bead7750fbf35
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-03 15:22:08 -0500

    #49 se ajusta el activity para el uso de multiples fragmentos

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ShowCheckinActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ShowCheckinActivity.groovy
@@ -5,13 +5,19 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v4.app.FragmentTransaction
+import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import com.makingdevs.mybarista.R
+import com.makingdevs.mybarista.common.MultiFragmentActivity
 import com.makingdevs.mybarista.common.SingleFragmentActivity
+import com.makingdevs.mybarista.ui.fragment.CommentsFragment
 import com.makingdevs.mybarista.ui.fragment.ShowCheckinFragment
 import groovy.transform.CompileStatic
 
 @CompileStatic
-public class ShowCheckinActivity extends SingleFragmentActivity {
+public class ShowCheckinActivity extends MultiFragmentActivity {
 
     static String EXTRA_CHECKIN_ID = "checkin_id"
 
@@ -21,11 +27,10 @@ public class ShowCheckinActivity extends SingleFragmentActivity {
         intent
     }
 
-    @Override
-    Fragment createFragment() {
+    Map createFragments(){
         String id = getIntent()?.extras.getSerializable(EXTRA_CHECKIN_ID)
         if(!id) throw new IllegalArgumentException("El checkin no tiene ID, mmmm tamales!")
-        new ShowCheckinFragment(id)
+        [top:new ShowCheckinFragment(id), middle:new CommentsFragment(), bottom:new CommentsFragment()]
     }
 
     @Override
