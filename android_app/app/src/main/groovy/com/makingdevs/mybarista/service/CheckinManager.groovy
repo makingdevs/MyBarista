f8f42e9c3cea618042ed9e2142f8fafdebbdbfd8
says <says.rodriguez@gmail.com>
2016-06-06 00:17:15 -0500

    #51 se agrega el metodo saveCicle para llamar al servicio de retrofit para el servicio del api de rails

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
@@ -1,9 +1,11 @@
 package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.command.CheckinCommand
+import com.makingdevs.mybarista.model.command.CircleFlavorCommand
 
 interface CheckinManager {
     void save(CheckinCommand checkin, Closure onSuccess, Closure onError)
     void list(Map params, Closure onSuccess, Closure onError)
     void show(String id, Closure onSuccess, Closure onError)
+    void saveCircle(CircleFlavorCommand circleFlavorCommand,Closure onSuccess, Closure onError)
 }
