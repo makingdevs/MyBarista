50043bfbabbe2e9aca4917cddee6f01940d4bf9f
says <says.rodriguez@gmail.com>
2016-06-06 05:26:58 -0500

    #53 se agregan valores por default al circulo del sabor

--- ruby_app/app/models/circle_flavor.rb
+++ ruby_app/app/models/circle_flavor.rb
@@ -1,2 +1,15 @@
 class CircleFlavor < ApplicationRecord
+	after_initialize :init
+  def init
+		self.sweetness ||= 0
+		self.acidity ||= 0
+		self.flowery ||= 0
+		self.spicy ||= 0
+		self.salty ||= 0
+		self.berries ||= 0
+		self.chocolate ||= 0
+		self.candy ||= 0
+		self.body ||= 0
+		self.cleaning ||= 0
+	end
 end
