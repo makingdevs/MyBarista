aa43dda7a2574e34d45379c5aeb8d8262e85c166
says <says.rodriguez@gmail.com>
2016-06-06 05:42:06 -0500

    #53 se agregan las modificaciones marcadas por el migration

--- ruby_app/db/schema.rb
+++ ruby_app/db/schema.rb
@@ -11,20 +11,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160603151537) do
+ActiveRecord::Schema.define(version: 20160606054654) do
 
-  create_table "checkins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
-    t.integer  "method",                             null: false
+  create_table "checkins", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "method",                                   null: false
     t.string   "origin"
-    t.decimal  "price",      precision: 8, scale: 2
+    t.decimal  "price",            precision: 8, scale: 2
     t.string   "note"
-    t.datetime "created_at",                         null: false
-    t.datetime "updated_at",                         null: false
+    t.datetime "created_at",                               null: false
+    t.datetime "updated_at",                               null: false
     t.integer  "user_id"
+    t.integer  "circle_flavor_id"
+    t.index ["circle_flavor_id"], name: "index_checkins_on_circle_flavor_id", using: :btree
     t.index ["user_id"], name: "index_checkins_on_user_id", using: :btree
   end
 
-  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "circle_flavors", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+    t.integer  "sweetness"
+    t.integer  "acidity"
+    t.integer  "flowery"
+    t.integer  "spicy"
+    t.integer  "salty"
+    t.integer  "berries"
+    t.integer  "chocolate"
+    t.integer  "candy"
+    t.integer  "body"
+    t.integer  "cleaning"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "body"
     t.integer  "user_id"
     t.integer  "checkin_id"
@@ -34,7 +51,7 @@ ActiveRecord::Schema.define(version: 20160603151537) do
     t.index ["user_id"], name: "index_comments_on_user_id", using: :btree
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "username"
     t.string   "token"
     t.datetime "created_at",      null: false
@@ -44,6 +61,7 @@ ActiveRecord::Schema.define(version: 20160603151537) do
     t.string   "lastName"
   end
 
+  add_foreign_key "checkins", "circle_flavors"
   add_foreign_key "checkins", "users"
   add_foreign_key "comments", "checkins"
   add_foreign_key "comments", "users"
