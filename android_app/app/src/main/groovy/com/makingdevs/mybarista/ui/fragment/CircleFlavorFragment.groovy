ad1465d0e369b2d7f8feeb4c42243105c519fa9b
says <says.rodriguez@gmail.com>
2016-06-06 00:19:08 -0500

    #51 se crean todos los elementos para obtener el proceso de cada slider del circulo del sabor y despues ser enviados al servicio de del api de rails

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/CircleFlavorFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/CircleFlavorFragment.groovy
@@ -13,7 +13,6 @@ import android.widget.SeekBar
 import android.widget.TextView
 import android.widget.Toast
 import com.makingdevs.mybarista.R
-import com.makingdevs.mybarista.model.Checkin
 import com.makingdevs.mybarista.model.CircleFlavor
 import com.makingdevs.mybarista.model.command.CircleFlavorCommand
 import com.makingdevs.mybarista.service.CheckinManager
@@ -28,8 +27,27 @@ public class CircleFlavorFragment extends Fragment {
     private static final String TAG = "CircleFlavorFragment"
     private static Context contextView
     private SeekBar seekProcessSweetness
-    private TextView seekProcessText
+    private TextView seekProcessSweetnessText
+    private SeekBar seekProcessAcidity
+    private TextView seekProcessAcidityText
+    private SeekBar seekProcessFlowery
+    private TextView seekProcessFloweryText
+    private SeekBar seekProcessSpicy
+    private TextView seekProcessSpicyText
+    private SeekBar seekProcessSalty
+    private TextView seekProcessSaltyText
+    private SeekBar seekProcessBerries
+    private TextView seekProcessBerriesText
+    private SeekBar seekProcessChocolate
+    private TextView seekProcessChocolateText
+    private SeekBar seekProcessCandy
+    private TextView seekProcessCandyText
+    private SeekBar seekProcessBody
+    private TextView seekProcessBodyText
+    private SeekBar seekProcessCleaning
+    private TextView seekProcessCleaningText
     private Button buttonCircleFlavor
+
 
     CheckinManager mCheckinManager = CheckingManagerImpl.instance
 
@@ -38,28 +56,166 @@ public class CircleFlavorFragment extends Fragment {
     @Override
     View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_circle_flavor, container, false)
-        seekProcessText = (TextView) root.findViewById(R.id.seekProcessSweetnessText)
+        seekProcessSweetnessText = (TextView) root.findViewById(R.id.seekProcessSweetnessText)
         seekProcessSweetness = (SeekBar) root.findViewById(R.id.seekSweetness)
-        seekProcessSweetness.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
-            public  void  onProgressChanged(SeekBar seekBar, int  progress,
-                                            boolean  fromUser) {
-                seekProcessText.setText("Sabor: "+progress);
-            }
-
-            public  void  onStartTrackingTouch(SeekBar seekBar) {}
-            public  void  onStopTrackingTouch(SeekBar seekBar) {}
-        });
+        seekProcessAcidityText = (TextView) root.findViewById(R.id.seekProcessAcidityText)
+        seekProcessAcidity = (SeekBar) root.findViewById(R.id.seekAcidity)
+        seekProcessFloweryText = (TextView) root.findViewById(R.id.seekProcessFloweryText)
+        seekProcessFlowery = (SeekBar) root.findViewById(R.id.seekFlowery)
+        seekProcessSpicyText = (TextView) root.findViewById(R.id.seekProcessSpacyText)
+        seekProcessSpicy = (SeekBar) root.findViewById(R.id.seekSpicy)
+        seekProcessSaltyText = (TextView) root.findViewById(R.id.seekProcessSaltyText)
+        seekProcessSalty = (SeekBar) root.findViewById(R.id.seekSalty)
+        seekProcessBerriesText = (TextView) root.findViewById(R.id.seekProcessBerriesText)
+        seekProcessBerries = (SeekBar) root.findViewById(R.id.seekBerries)
+        seekProcessChocolateText = (TextView) root.findViewById(R.id.seekProcessChocolateText)
+        seekProcessChocolate = (SeekBar) root.findViewById(R.id.seekChocolate)
+        seekProcessCandyText = (TextView) root.findViewById(R.id.seekProcessCandyText)
+        seekProcessCandy = (SeekBar) root.findViewById(R.id.seekCandy)
+        seekProcessBodyText = (TextView) root.findViewById(R.id.seekProcessBodyText)
+        seekProcessBody = (SeekBar) root.findViewById(R.id.seekBody)
+        seekProcessCleaningText = (TextView) root.findViewById(R.id.seekProcessCleaningText)
+        seekProcessCleaning = (SeekBar) root.findViewById(R.id.seekCleaning)
+        generedSeekSweetness()
+        generedSeekAcidity()
+        generedSeekFlowery()
+        generedSeekSpicy()
+        generedSeekSalty()
+        generedSeekBerries()
+        generedSeekChocolate()
+        generedSeekCandy()
+        generedSeekBody()
+        generedSeekCleaning()
         buttonCircleFlavor = (Button) root.findViewById(R.id.btnCircle)
         buttonCircleFlavor.setOnClickListener(new View.OnClickListener(){
             @Override
             void onClick(View v) {
-                getCircleFlavor()
-                //saveCircleFlavor(getCircleFlavor())
+                saveCircleFlavor(getCircleFlavor())
             }
         })
 
         contextView = getActivity().getApplicationContext()
         root
+    }
+
+    private void generedSeekSweetness() {
+        seekProcessSweetness.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessSweetnessText.setText("Dulzura "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekAcidity() {
+        seekProcessAcidity.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessAcidityText.setText("Acidez "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekFlowery() {
+        seekProcessFlowery.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessFloweryText.setText("Floral "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekSpicy() {
+        seekProcessSpicy.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessSpicyText.setText("Especiado  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekSalty() {
+        seekProcessSalty.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessSaltyText.setText("Salado  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekBerries() {
+        seekProcessBerries.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessBerriesText.setText("Frutos Rojos  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekChocolate() {
+        seekProcessChocolate.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessChocolateText.setText("Chocolate  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekCandy() {
+        seekProcessCandy.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessCandyText.setText("Caramelo  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekBody() {
+        seekProcessBody.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessBodyText.setText("Cuerpo  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
+    }
+
+    private void generedSeekCleaning() {
+        seekProcessCleaning.setOnSeekBarChangeListener(new  SeekBar.OnSeekBarChangeListener() {
+            public  void  onProgressChanged(SeekBar seekBar, int  progress,
+                                            boolean  fromUser) {
+                seekProcessCleaningText.setText("Limpieza  "+progress);
+            }
+
+            public  void  onStartTrackingTouch(SeekBar seekBar) {}
+            public  void  onStopTrackingTouch(SeekBar seekBar) {}
+        });
     }
 
     private void saveCircleFlavor(CircleFlavorCommand circleFlavorCommand) {
@@ -68,17 +224,16 @@ public class CircleFlavorFragment extends Fragment {
 
     private CircleFlavorCommand getCircleFlavor() {
         String dulzura = seekProcessSweetness.getProgress()
-        /*String acidez
-        String floral
-        String especiado
-        String salado
-        String frutosRojos
-        String chocolate
-        String caramelo
-        String cuerpo
-        String limpieza*/
-        Log.d(TAG,dulzura)
-        new CircleFlavorCommand()
+        String acidez = seekProcessAcidity.getProgress()
+        String floral = seekProcessFlowery.getProgress()
+        String especiado = seekProcessSpicy.getProgress()
+        String salado = seekProcessSalty.getProgress()
+        String frutosRojos = seekProcessBerries.getProgress()
+        String chocolate = seekProcessChocolate.getProgress()
+        String caramelo = seekProcessChocolate.getProgress()
+        String cuerpo = seekProcessBody.getProgress()
+        String limpieza = seekProcessCleaning.getProgress()
+        new CircleFlavorCommand(dulzura:dulzura,acidez:acidez,floral:floral,especiado:especiado,salado:salado,frutosRojos:frutosRojos,chocolate:chocolate,caramelo:caramelo,cuerpo:cuerpo,limpieza:limpieza)
     }
 
     private Closure onSuccess() {
