0545499e9bef910e51f0113f94fffd2fa349e946
says <says.rodriguez@gmail.com>
2016-06-06 05:26:18 -0500

    #53 se agrega la sentencia del belongsto de circleflavor a checkin y se agrega el argumento optional para no ser requerido

--- ruby_app/app/models/checkin.rb
+++ ruby_app/app/models/checkin.rb
@@ -2,4 +2,5 @@ class Checkin < ApplicationRecord
   enum method: [:Expresso,:Americano,:Goteo,:Prensa,:Sifón,:Otro ]
   has_many :comments
   belongs_to :user
+  belongs_to :circle_flavor, optional: true
 end
