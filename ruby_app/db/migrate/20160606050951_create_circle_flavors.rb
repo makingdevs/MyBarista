a1a31d62e5c53d7b6641bef9008df7730360ce35
says <says.rodriguez@gmail.com>
2016-06-06 00:42:05 -0500

    #53 se ajustan el tipo de variables que se requieren para crear el circulo del sabor en base de datos

--- ruby_app/db/migrate/20160606050951_create_circle_flavors.rb
+++ ruby_app/db/migrate/20160606050951_create_circle_flavors.rb
@@ -1,16 +1,16 @@
 class CreateCircleFlavors < ActiveRecord::Migration[5.0]
   def change
     create_table :circle_flavors do |t|
-      t.int :sweeness
-      t.int :acidity
-      t.int :flowery
-      t.int :spicy
-      t.int :salty
-      t.int :berries
-      t.int :chocolate
-      t.int :candy
-      t.int :body
-      t.int :cleaning
+      t.integer :sweeness
+      t.integer :acidity
+      t.integer :flowery
+      t.integer :spicy
+      t.integer :salty
+      t.integer :berries
+      t.integer :chocolate
+      t.integer :candy
+      t.integer :body
+      t.integer :cleaning
 
       t.timestamps
     end
