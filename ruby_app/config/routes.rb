38d39aae67ce0e36d7304a5d51da5bb7fdf7f163
says <says.rodriguez@gmail.com>
2016-06-07 10:15:18 -0500

    #56 se agrega el resource de el controller de circle

--- ruby_app/config/routes.rb
+++ ruby_app/config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+
   resources :comments
   resources :checkins
   resources :users
+  resources :circles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    get '/login/user/', to: 'users#login', as: 'login'
-   post 'checkins/:id/circleFlavor', to: 'checkins#circleFlavor', as: 'circleFlavor'
+   post 'checkins/:id/circleFlavor', to: 'circles#create', as: 'circleFlavor'
    post '/users/image/profile', to: 'users#imageProfile', as: 's3'
 end
