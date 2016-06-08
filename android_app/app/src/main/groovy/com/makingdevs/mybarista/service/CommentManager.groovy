a60aaaa63513611ecc8addd23a3a78b4cfc15336
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-08 09:32:31 -0500

    #57 creamdo la interface del administrador de comentarios

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CommentManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CommentManager.groovy
@@ -1,6 +1,10 @@
 package com.makingdevs.mybarista.service
 
+import com.makingdevs.mybarista.model.command.CommentCommand
+
 interface CommentManager {
 
     void list(Map params, Closure onSuccess, Closure onError)
+    void save(CommentCommand comment, Closure onSuccess, Closure onError)
+
 }
