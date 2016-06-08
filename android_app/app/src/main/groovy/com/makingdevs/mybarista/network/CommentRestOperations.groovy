d3a80ae6886b8fd1a571d2a851087f07381544b7
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-08 09:32:08 -0500

    #57 creando el comment operation rest

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CommentRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CommentRestOperations.groovy
@@ -1,13 +1,19 @@
 package com.makingdevs.mybarista.network
 
 import com.makingdevs.mybarista.model.Comment
+import com.makingdevs.mybarista.model.command.CommentCommand
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 import retrofit2.http.QueryMap
 
 public interface CommentRestOperations {
 
     @GET("comments")
     Call<List<Comment>> getComments(@QueryMap Map<String, String> options)
+
+    @POST("checkins")
+    Call<Comment> createComment(@Body CommentCommand comment)
 
 }
