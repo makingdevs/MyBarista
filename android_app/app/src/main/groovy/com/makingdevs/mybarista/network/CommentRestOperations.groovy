9a5b0e691b4aa680f250bff3fba1a2172592f12f
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-09 10:25:58 -0500

    #62 se ajusta el endpoint al que se apunta para ir por los comentarios de un checkin

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CommentRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/CommentRestOperations.groovy
@@ -6,12 +6,13 @@ import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.QueryMap
 
 public interface CommentRestOperations {
 
-    @GET("comments")
-    Call<List<Comment>> getComments(@QueryMap Map<String, String> options)
+    @GET("checkins/{id}/comments")
+    Call<List<Comment>> getComments(@Path("id") String id)
 
     @POST("comments")
     Call<Comment> createComment(@Body CommentCommand comment)
