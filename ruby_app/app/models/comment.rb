7bafe0814ec2549e3c8e0506e192515e7e5d71af
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-09 13:35:31 -0500

    #63 se agrega en el modelo el modo de rendereo de json para el modelo de comenatrio

--- ruby_app/app/models/comment.rb
+++ ruby_app/app/models/comment.rb
@@ -1,4 +1,12 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :checkin
+
+  def as_json(options={})
+    super(:only => [:body,:created_at],
+        :include => {
+        :user => {:only => [:username]},
+      }
+    )
+  end
 end
