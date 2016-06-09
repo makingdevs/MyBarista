b43104be94aafd98415abab40dd1465d0ba3045a
says33 <says.rodriguez@gmail.com>
2016-06-09 15:38:27 -0500

    #59 se crea el fragmento de ratingCoffee para  agregar los metodos de la obtencion del proceso y su persistencia

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/RatingCoffeFragment.groovy
@@ -0,0 +1,12 @@
+package com.makingdevs.mybarista.ui.fragment
+
+import android.app.Fragment
+import groovy.transform.CompileStatic
+
+@CompileStatic
+public class RatingCoffeFragment extends Fragment{
+
+
+    
+
+}
