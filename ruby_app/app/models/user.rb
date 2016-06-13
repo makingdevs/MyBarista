e89aeeee24cf60068a62b16f8c156f7ced4fb56b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-13 15:30:08 -0500

    #74 agregando checkins totales al user

--- ruby_app/app/models/user.rb
+++ ruby_app/app/models/user.rb
@@ -3,4 +3,16 @@ class User < ApplicationRecord
   has_many :checkins
   has_many :comments
   has_secure_password
+
+
+  def as_json(options={})
+    super(
+      :methods => [:checkins_count]
+    )
+  end
+
+  def checkins_count
+    checkins.count
+  end
+
 end
