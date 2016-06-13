4bd826fb1d5cb159a852ea0b0a19238948abc275
Francisco Reyes <francisco@makingdevs.com>
2016-06-13 12:09:28 -0500

    #72 Se agrega modelo PhotoCheckin

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/PhotoCheckin.groovy
@@ -0,0 +1,12 @@
+package com.makingdevs.mybarista.model
+
+class PhotoCheckin{
+
+    String id
+    String url_file
+    String name_file
+    String created_at
+    String updated_at
+    String user_id
+
+}
