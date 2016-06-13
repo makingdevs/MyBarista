1b061e8f6c797e80a358ac49b5b8d24e1d298818
Francisco Reyes <francisco@makingdevs.com>
2016-06-13 17:43:09 -0500

    #77 Se agrega ruta anidada para consultar foto de checkin dado un usuario

--- ruby_app/config/routes.rb
+++ ruby_app/config/routes.rb
@@ -4,11 +4,15 @@ Rails.application.routes.draw do
   resources :checkins
   resources :users
   resources :circles
+
+  resources :users do
+    resources :checkins
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
    get '/login/user/', to: 'users#login', as: 'login'
    post 'checkins/:id/circleFlavor', to: 'circles#create', as: 'circleFlavor'
    post '/users/image/profile', to: 'users#imageProfile', as: 's3'
    get '/checkins/:id/comments', to: 'checkins#comments', as: 'checkinsComments'
-   get '/users/photo/:id', to:'users#photo_url_s3', as: 'photo_s3'
+   get '/users/:user_id/checkins/:checkin_id/photo', to:'users#photo_url_s3', as: 'photo_s3'
 end
