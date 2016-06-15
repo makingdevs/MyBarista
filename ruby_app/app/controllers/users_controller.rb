33859f2b0ab0f79962f5b021f124b1872f330cdb
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-15 10:28:33 -0500

    #82 agregando metodo de busqueda en el controller de user

--- ruby_app/app/controllers/users_controller.rb
+++ ruby_app/app/controllers/users_controller.rb
@@ -78,6 +78,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def search
+    @users = User.search(params[:search])
+    render json: @users
+  end
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_user
