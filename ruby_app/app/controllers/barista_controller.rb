806000f3db0918c7389242d5ad1d4066d2b53480
says <says.rodriguez@gmail.com>
2016-06-16 10:29:17 -0500

    #68 se agregan los metodos de show , create y metodos privados de controller de barista

--- ruby_app/app/controllers/barista_controller.rb
+++ ruby_app/app/controllers/barista_controller.rb
@@ -1,2 +1,30 @@
 class BaristaController < ApplicationController
+  before_action :set_barista, only: [:show]
+
+  def create
+    @checkin = Checkin.find(params['id'])
+    puts @checkin
+    @baristum = Baristum.new(barista_params)
+    @checkin.baristum = @baristum
+    if @checkin.save
+      render json: @baristum, status: :created, location: @checkin
+    else
+      render json: @baristum.errors, status: :unprocessable_entity
+    end
+  end
+
+  def show
+    render json: @baristum
+  end
+
+  private
+    def set_barista
+      @baristum = Baristum.find(params[:id])
+    end
+
+    def barista_params
+      params.permit(:name)
+    end
+
+
 end
