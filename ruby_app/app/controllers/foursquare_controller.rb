8e8b1d7600394a5bbb5ab7bdd0224750ffb5df8d
Francisco Reyes <francisco@makingdevs.com>
2016-06-16 11:49:42 -0500

    #90 Se agrega metodo para buscar lugares dado una latitud, longitud, consulta

--- ruby_app/app/controllers/foursquare_controller.rb
+++ ruby_app/app/controllers/foursquare_controller.rb
@@ -1,11 +1,5 @@
 class FoursquareController < ApplicationController
   def index
-  	current_date = Time.now.strftime("%Y%m%d")
-  	client = Foursquare2::Client.new(:api_version => current_date,
-  		:client_id => '', 
-  		:client_secret => '')
-  	client.user(201716897)
-  	render json: client
   end
 
   def show
@@ -15,5 +9,15 @@ class FoursquareController < ApplicationController
   end
 
   def update
+  end
+
+  def searh_venues
+  	current_date = Time.now.strftime("%Y%m%d")
+  	client = Foursquare2::Client.new(:api_version => current_date,
+  		:client_id => Rails.application.secrets.foursquare_id, 
+  		:client_secret => Rails.application.secrets.foursquare_secret)
+  	venues_near = client.search_venues(:ll => "#{params[:latitude]},#{params[:longitude]}", :query => params[:query]) 
+  	#p "#{venues_near.venues.first.id}"
+    render :json => venues_near.venues
   end
 end
