663df5e61e8062f3d15e024f2267a1fcee3dc94f
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-17 15:28:21 -0500

    #99 super refactor del fragmento de show

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowCheckinFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowCheckinFragment.groovy
@@ -12,6 +12,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.ViewStub
 import android.widget.*
 import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.common.CamaraUtil
@@ -20,7 +21,6 @@ import com.makingdevs.mybarista.model.Checkin
 import com.makingdevs.mybarista.model.PhotoCheckin
 import com.makingdevs.mybarista.model.User
 import com.makingdevs.mybarista.model.command.UploadCommand
-import com.makingdevs.mybarista.model.command.UserCommand
 import com.makingdevs.mybarista.service.*
 import com.makingdevs.mybarista.ui.activity.BaristaActivity
 import com.makingdevs.mybarista.ui.activity.CircleFlavorActivity
@@ -32,10 +32,16 @@ import retrofit2.Response
 public class ShowCheckinFragment extends Fragment {
 
     CheckinManager mCheckinManager = CheckingManagerImpl.instance
-    static final int REQUEST_TAKE_PHOTO = 0
+    SessionManager mSessionManager = SessionManagerImpl.instance
+    UserManager mUserManager = UserManagerImpl.instance
 
+    static final int REQUEST_TAKE_PHOTO = 0
     private static final String TAG = "ShowCheckinFragment"
     private static String ID_CHECKIN
+
+    CamaraUtil mCamaraUtil = new CamaraUtil()
+    ImageUtil mImageUtil1 = new ImageUtil()
+
     TextView mOrigin
     TextView mMethod
     TextView mPrice
@@ -47,14 +53,10 @@ public class ShowCheckinFragment extends Fragment {
     ImageButton mButtonCamera
     File photoFile
     ImageUtil mImageUtil
-    SessionManager mSessionManager = SessionManagerImpl.instance
     User currentUser
     ImageView photoCheckinImageView
-    CamaraUtil mCamaraUtil = new CamaraUtil()
-    ImageUtil mImageUtil1 = new ImageUtil()
     Button mBarista
-
-    UserManager mUserManager = UserManagerImpl.instance
+    String mCheckinId
 
     ShowCheckinFragment(String id){
         Bundle args = new Bundle()
@@ -67,72 +69,42 @@ public class ShowCheckinFragment extends Fragment {
         super.onCreate(savedInstanceState)
         if(!getArguments() || !getArguments()?.getSerializable(ID_CHECKIN))
             throw new IllegalArgumentException("No arguments $ID_CHECKIN")
-        String checkinId = getArguments()?.getSerializable(ID_CHECKIN)
-        mCheckinManager.show(checkinId,onSuccess(),onError())
+        mCheckinId = getArguments()?.getSerializable(ID_CHECKIN)
     }
-
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
-
-            //Log.d(TAG,"Resize img...")
-            Bitmap bitmapResize = mCamaraUtil.resizeBitmapFromFilePath(photoFile.getPath(),1280,960)
-            File photo = mCamaraUtil.saveBitmapToFile(bitmapResize,photoFile.getName())
-
-            currentUser = mSessionManager.getUserSession(getContext())
-            //Log.d(TAG,"Usuario..."+currentUser.dump().toString())
-
-            //Log.d(TAG,"Enviando...")
-            String checkinId = getArguments()?.getSerializable(ID_CHECKIN)
-            mUserManager.upload(new UploadCommand(idCheckin: checkinId,idUser:currentUser.id,pathFile: photo.getPath()),onSuccessFile(),onError())
-
-            //Log.d(TAG,"Galeria...")
-            mImageUtil.addPictureToGallery(getContext(),photo.getPath())
-
-        } else {
-            Toast.makeText(getContext(), "Error al caputar la foto", Toast.LENGTH_SHORT).show()
-        }
-    }
-
 
     @Override
     View onCreateView(LayoutInflater inflater,@Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_show_chek_in, container, false)
-        itemView = root
-        mButtonCircleFlavor = (Button) root.findViewById(R.id.btnCircle_flavor)
+        itemView = inflater.inflate(R.layout.fragment_show_chek_in, container, false)
+        findingElements()
+        mUserManager.getPhoto(mCheckinId,onSuccessPhoto(),onError())
+        mCheckinManager.show(mCheckinId,onSuccess(),onError())
+        itemView
+    }
 
-        String currentCheckin = getArguments()?.getSerializable(ID_CHECKIN)
-        String photoURL = mUserManager.getPhoto(currentCheckin,onSuccessGetPhoto(),onError())
+    private void findingElements(){
+        mButtonCircleFlavor = (Button) itemView.findViewById(R.id.btnCircle_flavor)
+        mBarista = (Button) itemView.findViewById(R.id.btnBarista)
+        mOrigin = (TextView) itemView.findViewById(R.id.origin_data)
+        mMethod = (TextView) itemView.findViewById(R.id.method_data)
+        mPrice = (TextView) itemView.findViewById(R.id.price_data)
+        mNote = (TextView) itemView.findViewById(R.id.note_data)
+        mBaristaName = (TextView) itemView.findViewById(R.id.barista_name_data)
+        mButtonCamera = (ImageButton) itemView.findViewById(R.id.button_camera)
+        photoCheckinImageView = (ImageView) itemView.findViewById(R.id.show_photo_checkin)
+        //mDateCreated  = (TextView) itemView.findViewById(R.id._data)
+    }
 
-        mBarista = (Button) root.findViewById(R.id.btnBarista)
-        photoCheckinImageView = (ImageView) root.findViewById(R.id.show_photo_checkin)
-        mImageUtil1.setPhotoImageView(getContext(),photoURL,photoCheckinImageView)
-
-        mBarista.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String checkinId = getArguments()?.getSerializable(ID_CHECKIN)
-                Intent intent = BaristaActivity.newIntentWithContext(getContext())
-                intent.putExtra("checkingId",checkinId)
-                startActivity(intent)
-            }
-        });
-
-        mButtonCircleFlavor.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                String checkinId = getArguments()?.getSerializable(ID_CHECKIN)
-                Intent intent = CircleFlavorActivity.newIntentWithContext(getContext())
-                intent.putExtra("checkingId",checkinId)
-                startActivity(intent)
-            }
-        });
-        root
+    private void setCheckinInView(Checkin checkin){
+        showElements()
+        mOrigin.text = checkin.origin
+        mMethod.text = checkin.method
+        mPrice.text = checkin.price
+        mNote.text = checkin.note
+        mBaristaName.text = checkin?.baristum?.name ?: ""
     }
 
     private Closure onSuccess(){
         { Call<Checkin> call, Response<Checkin> response ->
-            Log.d(TAG,response.body().toString())
             setCheckinInView(response.body())
         }
     }
@@ -141,46 +113,17 @@ public class ShowCheckinFragment extends Fragment {
         { Call<Checkin> call, Throwable t -> Log.d("ERRORZ", "el error "+t.message) }
     }
 
-    private Closure onSuccessFile(){
-        { Call<Checkin> call, Response<Checkin> response ->
-            Log.d(TAG,response.dump().toString())
-            String currentCheckin = getArguments()?.getSerializable(ID_CHECKIN)
-            String photoURL = mUserManager.getPhoto(currentCheckin,onSuccessGetPhoto(),onError())
-            mImageUtil1.setPhotoImageView(getContext(),photoURL,photoCheckinImageView)
-        }
-    }
-
-    private Closure onSuccessGetPhoto(){
+    private Closure onSuccessPhoto(){
         { Call<PhotoCheckin> call, Response<PhotoCheckin> response ->
-
             if(response.body()!=null){
-                Log.d(TAG,"URL ARCHIVO SERVIDOR... "+response.body().dump().toString())
                 String photoUrl = response.body().url_file
                 mImageUtil1.setPhotoImageView(getContext(),photoUrl,photoCheckinImageView)
             }
             else{
+                //poner image default
                 Log.d(TAG,"NO HAY FOTO")
             }
         }
-    }
-
-    private void setCheckinInView(Checkin checkin){
-        mOrigin = (TextView) itemView.findViewById(R.id.origin_data)
-        mMethod = (TextView) itemView.findViewById(R.id.method_data)
-        mPrice = (TextView) itemView.findViewById(R.id.price_data)
-        mNote = (TextView) itemView.findViewById(R.id.note_data)
-        mBaristaName = (TextView) itemView.findViewById(R.id.barista_name_data)
-        //mDateCreated  = (TextView) itemView.findViewById(R.id._data)
-        mButtonCamera = (ImageButton) itemView.findViewById(R.id.button_camera)
-        mButtonCamera.onClickListener = {
-            dispatchTakePictureIntent()
-        }
-
-        mOrigin.text = checkin.origin
-        mMethod.text = checkin.method
-        mPrice.text = checkin.price
-        mNote.text = checkin.note
-        mBaristaName.text = checkin?.baristum?.name ?: ""
     }
 
     private void dispatchTakePictureIntent() {
@@ -193,10 +136,43 @@ public class ShowCheckinFragment extends Fragment {
             }
             if (photoFile != null) {
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(photoFile))
-                Log.d(TAG,takePictureIntent.getProperties().toString())
                 startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO)
             }
         }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == Activity.RESULT_OK) {
+            Bitmap bitmapResize = mCamaraUtil.resizeBitmapFromFilePath(photoFile.getPath(),1280,960)
+            File photo = mCamaraUtil.saveBitmapToFile(bitmapResize,photoFile.getName())
+            currentUser = mSessionManager.getUserSession(getContext())
+            mUserManager.upload(new UploadCommand(idCheckin: mCheckinId,idUser:currentUser.id,pathFile: photo.getPath()),onSuccessPhoto(),onError())
+            mImageUtil.addPictureToGallery(getContext(),photo.getPath())
+        } else {
+            Toast.makeText(getContext(), "Error al caputar la foto", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    private bindingElements(){
+        mButtonCamera.onClickListener = { dispatchTakePictureIntent() }
+        mBarista.onClickListener = {
+            Intent intent = BaristaActivity.newIntentWithContext(getContext())
+            intent.putExtra("checkingId",mCheckinId)
+            startActivity(intent)
+        }
+        mButtonCircleFlavor.onClickListener = {
+            Intent intent = CircleFlavorActivity.newIntentWithContext(getContext())
+            intent.putExtra("checkingId",mCheckinId)
+            startActivity(intent)
+        }
+    }
+
+    private void showElements(){
+        ViewStub stub = (ViewStub) itemView.findViewById(R.id.stub_bottons)
+        stub.inflate()
+        findingElements()
+        bindingElements()
     }
 
 }
