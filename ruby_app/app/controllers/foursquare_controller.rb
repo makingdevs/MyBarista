248a8889710e7071677b4fd80716bb0b39bbc436
Francisco Reyes <francisco@makingdevs.com>
2016-06-17 16:26:42 -0500

    #101 Se agrega metodo para buscar venue por id en foursquare

--- ruby_app/app/controllers/foursquare_controller.rb
+++ ruby_app/app/controllers/foursquare_controller.rb
@@ -1,4 +1,12 @@
 class FoursquareController < ApplicationController
+
+  def initialize
+    current_date = Time.now.strftime("%Y%m%d")
+    @client = Foursquare2::Client.new(:api_version => current_date,
+      :client_id => Rails.application.secrets.foursquare_id, 
+      :client_secret => Rails.application.secrets.foursquare_secret)
+  end
+  
   def index
   end
 
@@ -19,5 +27,12 @@ class FoursquareController < ApplicationController
   	venues_near = client.search_venues(:ll => "#{params[:latitude]},#{params[:longitude]}", :query => params[:query]) 
   	#p "#{venues_near.venues.first.id}"
     render :json => venues_near.venues
+  end
+
+  def search_venue_by_id
+    venue_detail = @client.venue(params[:venue_id])
+    if venue_detail != nil
+      render json:venue_detail
+    end
   end
 end
