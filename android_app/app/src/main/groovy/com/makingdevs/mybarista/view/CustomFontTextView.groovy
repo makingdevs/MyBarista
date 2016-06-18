ce208ae7487efa33d5f2571fbfb6abfacaefdcc3
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-18 13:06:44 -0500

    #103 agregando clase para la vista personalizada

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/view/CustomFontTextView.groovy
@@ -0,0 +1,56 @@
+package com.makingdevs.mybarista.view
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.util.AttributeSet;
+import android.widget.TextView
+import com.makingdevs.mybarista.R;
+
+
+public class CustomFontTextView extends TextView{
+
+    private String mFont;
+
+    public CustomFontTextView(Context context) {
+        super(context);
+        init(null);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(attrs);
+    }
+
+    public CustomFontTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(attrs);
+    }
+
+    private void init(AttributeSet attrs) {
+        if (attrs != null) {
+            obtainAttributes(attrs);
+
+            if (mFont  != null)
+                setFont(mFont);
+        }
+    }
+
+    private void obtainAttributes(AttributeSet attrs) {
+        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomFontTextView);
+        mFont = a.getString(R.styleable.CustomFontTextView_fontName);
+
+        a.recycle();
+    }
+
+    private Typeface getTypeFace(Context context, String font) {
+        return Typeface.createFromAsset(context.getAssets(), font);
+    }
+
+    public void setFont(String font) {
+        mFont = font;
+
+        Typeface typeface = getTypeFace(getContext(), font);
+        setTypeface(typeface);
+    }
+}
