01f7f8549a90ff129226de1397d35f507ad0b611
Francisco Reyes <francisco@makingdevs.com>
2016-06-19 22:19:11 -0500

    #101 Se agrega metodo para buscar en base de datos el lugar, asi como cambiar el nombre de un metodo que busca en foursquare

--- ruby_app/app/controllers/checkins_controller.rb
+++ ruby_app/app/controllers/checkins_controller.rb
@@ -17,8 +17,10 @@ class CheckinsController < ApplicationController
 
   # POST /checkins
   def create
-    save_venue(params['idVenueFoursquare'])
-    #@checkin = Checkin.new(checkin_params)
+    @venue = search_venue(params['idVenueFoursquare'])
+    if @venue == nil
+      @venue = save_venue(params['idVenueFoursquare'])
+    end
     #user = User.find_by username: params['username']
     #@checkin.user = user
     #if @checkin.save
@@ -48,8 +50,15 @@ class CheckinsController < ApplicationController
     end
   end
 
+  def search_venue(venue_id)
+    if venue_id != nil
+      @venue = Venue.find_by venue_id_foursquare: venue_id
+      return @venue
+    end
+  end
+
   def save_venue(venue_id)
-    venue_detail = venue_id != nil ? search_venue_by_id(venue_id) : "venue_id_nil"
+    venue_detail = venue_id != nil ? search_venue_foursquare_by_id(venue_id) : "venue_id_nil"
     if venue_detail != "venue_id_nil"
       @venue = Venue.create(venue_id_foursquare: venue_id, name:venue_detail.name,formatted_address: venue_detail.location.formattedAddress.join(''))
       if @venue
@@ -58,11 +67,12 @@ class CheckinsController < ApplicationController
     end
   end
 
-  def search_venue_by_id(venue_id)
+  def search_venue_foursquare_by_id(venue_id)
     current_date = Time.now.strftime("%Y%m%d")
     @client = Foursquare2::Client.new(:api_version => current_date,
       :client_id => Rails.application.secrets.foursquare_id, 
-      :client_secret => Rails.application.secrets.foursquare_secret)
+      :client_secret => Rails.application.secrets.foursquare_secret,
+      :ssl => { :verify =>false})
     venue_detail = @client.venue(venue_id)
     if venue_detail != nil
       return venue_detail
