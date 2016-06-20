249fdd48d7ace37fefd6dcc2f6cdfb1bd2c255cd
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-20 08:10:13 -0500

    #104 se quita la llamada de la creacion del menu en el activity de lista de bebidas por usuario

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewByUserActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewByUserActivity.groovy
@@ -29,6 +29,5 @@ public class ListBrewByUserActivity extends SingleFragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        createMenu()
     }
 }
