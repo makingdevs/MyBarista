d25ba9acebc1f184ea447a062d313e297d700aef
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-20 08:10:35 -0500

    #104 agregando imports de profile

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ProfileActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ProfileActivity.groovy
@@ -2,6 +2,8 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.os.Bundle
+import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.ProfileFragment
