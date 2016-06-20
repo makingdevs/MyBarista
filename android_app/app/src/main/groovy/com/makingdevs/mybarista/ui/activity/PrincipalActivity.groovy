8a67ff3beedddcfdbf9036218d29237f3638205b
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-20 14:59:50 -0500

    #108 ajustando los colores de la barra de navegacion y ajustando el veneto de back

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/PrincipalActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/PrincipalActivity.groovy
@@ -2,6 +2,7 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
@@ -9,6 +10,7 @@ import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentTransaction
 import android.support.v7.app.AppCompatActivity
 import android.util.Log
+import android.view.KeyEvent
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigation
 import com.aurelhubert.ahbottomnavigation.AHBottomNavigationItem
 import com.makingdevs.mybarista.R
@@ -19,7 +21,7 @@ import com.makingdevs.mybarista.ui.fragment.SearchUserFragment
 class PrincipalActivity extends AppCompatActivity {
 
     static Intent newIntentWithContext(Context context) {
-        Intent intent = new Intent(context, BaristaActivity)
+        Intent intent = new Intent(context, PrincipalActivity)
         intent
     }
 
@@ -35,13 +37,16 @@ class PrincipalActivity extends AppCompatActivity {
 
         AHBottomNavigation bottomNavigation = (AHBottomNavigation) findViewById(R.id.bottom_navigation);
 
-        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Check-ins", R.drawable.ic_foursquar, R.color.primary)
-        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Busqueda", R.drawable.ic_search, R.color.primary)
-        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Perfil", R.drawable.ic_profile, R.color.primary)
+        AHBottomNavigationItem item1 = new AHBottomNavigationItem("Check-ins", R.drawable.ic_foursquar)
+        AHBottomNavigationItem item2 = new AHBottomNavigationItem("Busqueda", R.drawable.ic_search)
+        AHBottomNavigationItem item3 = new AHBottomNavigationItem("Perfil", R.drawable.ic_profile)
 
         bottomNavigation.addItem(item1)
         bottomNavigation.addItem(item2)
         bottomNavigation.addItem(item3)
+
+        bottomNavigation.setAccentColor(Color.parseColor("#0288D1"))
+        bottomNavigation.setInactiveColor(Color.parseColor("#727272"))
 
         bottomNavigation.onTabSelectedListener = { int position, boolean wasSelected ->
             if (position == 0 && !wasSelected){
@@ -62,5 +67,14 @@ class PrincipalActivity extends AppCompatActivity {
         transaction.replace(R.id.container, fragment)
         transaction.addToBackStack(null)
         transaction.commit()
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if ((keyCode == KeyEvent.KEYCODE_BACK)) {
+            moveTaskToBack(true)
+            return true
+        }
+        return super.onKeyDown(keyCode, event)
     }
 }
