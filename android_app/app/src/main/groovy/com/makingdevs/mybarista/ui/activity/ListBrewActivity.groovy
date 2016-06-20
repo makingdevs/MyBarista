ee63b5487eb172ff5adec0e9543671a2bb7bd881
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-20 08:09:22 -0500

    #104 se ajusta el uso del menu en el activity de la lista de bebidas

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ListBrewActivity.groovy
@@ -5,6 +5,9 @@ import android.content.Intent
 import android.os.Bundle
 import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
+import android.view.Menu
+import android.view.MenuItem
+import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.ListBrewFragment
 import groovy.transform.CompileStatic
@@ -25,6 +28,36 @@ public class ListBrewActivity extends SingleFragmentActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        createMenu()
+        setActivityToolbarVisible(true)
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu)
+        return true
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Integer id = item.getItemId()
+
+        //TODO refactor de esto hay una mejor manera
+        if (id == R.id.menu_profile) {
+            Intent intent = ProfileActivity.newIntentWithContext(this)
+            startActivity(intent)
+        }
+        if(id == R.id.menu_search) {
+            Intent intent = SearchUserActivity.newIntentWithContext(this)
+            startActivity(intent)
+        }
+        if(id == R.id.menu_logout) {
+            mSessionManager.setLogout(this)
+            Intent intent = LoginActivity.newIntentWithContext(this)
+            startActivity(intent)
+            finish()
+        }
+
+        super.onOptionsItemSelected(item)
+    }
+
 }
