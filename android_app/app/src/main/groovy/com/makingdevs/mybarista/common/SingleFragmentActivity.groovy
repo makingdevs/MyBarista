6e69998e1044b8b332d8c31598fa52e5a771ff6a
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-22 15:20:26 -0500

    #116 eliminando el menu en el activity principal

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/SingleFragmentActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/SingleFragmentActivity.groovy
@@ -5,8 +5,6 @@ import android.support.annotation.Nullable
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
-import android.view.View
 import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.service.SessionManager
 import com.makingdevs.mybarista.service.SessionManagerImpl
@@ -16,7 +14,6 @@ import groovy.transform.CompileStatic
 abstract class SingleFragmentActivity extends AppCompatActivity implements WithFragment {
 
     SessionManager mSessionManager = SessionManagerImpl.instance
-    Toolbar mToolbar;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,24 +23,6 @@ abstract class SingleFragmentActivity extends AppCompatActivity implements WithF
         Fragment fragment = fm.findFragmentById(R.id.container)
         if (!fragment)
             fm.beginTransaction().add(R.id.container, createFragment()).commit()
-
-        setToolbar()
-
     }
 
-    def setToolbar(){
-        mToolbar = (Toolbar) findViewById(R.id.toolbar)
-        mToolbar.navigationOnClickListener = { onNavigationButtonClicked() }
-        if (mToolbar != null) {
-            setSupportActionBar(mToolbar)
-        }
-    }
-
-    def setActivityToolbarVisible(boolean visible){
-        mToolbar.setVisibility(visible? View.VISIBLE :  View.INVISIBLE )
-    }
-
-    def onNavigationButtonClicked() {
-        finish()
-    }
 }
