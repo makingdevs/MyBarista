0b101d17cbf5cf99ede8da3daafd04b2d62297cb
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-22 11:02:05 -0500

    #115 agregando atributo al command de correo y ajustando validacion

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/RegistrationCommand.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/RegistrationCommand.groovy
@@ -5,10 +5,11 @@ class RegistrationCommand{
     String username
     String password
     String confirmPassword
+    String email
 
     Boolean validateCommand(){
         def pattern = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[A-Za-z]{2,4}/
-        this.username ==~ pattern && this.password == this.confirmPassword && this.username != this.password
+        this.email ==~ pattern && this.password == this.confirmPassword && this.email != this.password
     }
 
 }
