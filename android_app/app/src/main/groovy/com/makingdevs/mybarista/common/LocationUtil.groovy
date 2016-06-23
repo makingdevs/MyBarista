cbe213f9cd86291b4c7d906a80337ba4d46c3464
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-06-23 12:12:52 -0500

    #50 ajustando porqueri.... ajusatndo una validacion del jefe francisco jijijijijij

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/LocationUtil.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/LocationUtil.groovy
@@ -43,7 +43,7 @@ class LocationUtil implements GoogleApiClient.ConnectionCallbacks, GoogleApiClie
     @Override
     void onConnected(@Nullable Bundle bundle) {
         mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient)
-        if (!mLastLocation) {
+        if (mLastLocation) {
             mGPSLocation.setLatitude(mLastLocation.latitude)
             mGPSLocation.setLongitude(mLastLocation.longitude)
         }
