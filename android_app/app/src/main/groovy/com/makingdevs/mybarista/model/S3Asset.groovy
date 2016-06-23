c2f438be52bf67110214468809669e480b59751f
says <says.rodriguez@gmail.com>
2016-06-23 17:32:52 -0500

    #118 se agrega la nueva clase del modelo s3 asset

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/S3Asset.groovy
@@ -0,0 +1,13 @@
+package  com.makingdevs.mybarista.model
+
+import groovy.beans.Bindable
+import groovy.transform.CompileStatic
+import groovy.transform.EqualsAndHashCode
+
+@CompileStatic
+@EqualsAndHashCode
+class S3_Asset{
+
+    @Bindable String url_file
+
+}
