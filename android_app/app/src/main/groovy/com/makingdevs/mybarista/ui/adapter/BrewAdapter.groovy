95f0fb7244b5cb424460a677b9e804d1cd98220c
Francisco Reyes <francisco@makingdevs.com>
2016-06-27 15:02:46 -0500

    #135 Se agrega signo de pesos al precio del cafe

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/adapter/BrewAdapter.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/adapter/BrewAdapter.groovy
@@ -67,7 +67,7 @@ class BrewAdapter extends RecyclerView.Adapter<BrewViewHolder>{
             morigin.text = checkin.origin
             String fecha = DateUtils.getRelativeTimeSpanString(mCheckin.created_at.time, new Date().time, 0).toString()
             mmoment.text = fecha
-            mprice.text = checkin.price
+            mprice.text = "\$ ${checkin.price}"
             mrating.setRating(Float.parseFloat(checkin.rating ?: "0"))
             if(mCheckin.s3_asset?.url_file)
                 mImageUtil1.setPhotoImageView(mContext, mCheckin.s3_asset?.url_file,photoCheckinImageView)
