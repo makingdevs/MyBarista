fde5f89d7a3fea2e4b5051b811a68c2c4f79594c
Francisco Reyes <francisco@makingdevs.com>
2016-06-27 15:20:11 -0500

    #132 Se agrega modelo para subir foto barista

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/model/command/UploadPhotoBaristaCommand.groovy
@@ -0,0 +1,11 @@
+package com.makingdevs.mybarista.model.command
+
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class UploadPhotoBaristaCommand {
+
+    String idBarista
+    String pathFile
+
+}
