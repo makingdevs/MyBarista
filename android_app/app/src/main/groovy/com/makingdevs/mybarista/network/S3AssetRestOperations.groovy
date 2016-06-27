d29c297fe21fadb7eeecc0449d6fca5a2cbdcb0e
Francisco Reyes <francisco@makingdevs.com>
2016-06-27 15:15:16 -0500

    #132 Se agrega metodo para subir foto de barista

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/network/S3AssetRestOperations.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/network/S3AssetRestOperations.groovy
@@ -1,5 +1,6 @@
 package com.makingdevs.mybarista.network
 
+import com.makingdevs.mybarista.model.PhotoBarista
 import com.makingdevs.mybarista.model.PhotoCheckin
 import okhttp3.MultipartBody
 import okhttp3.RequestBody
@@ -12,4 +13,7 @@ public interface S3AssetRestOperations {
     @POST("/checkin/photo/save")
     Call<PhotoCheckin> uploadImage(@Part("checkin") RequestBody checkin, @Part("user") RequestBody user, @Part MultipartBody.Part file)
 
+    @Multipart
+    @POST("/barista/photo/save_photo")
+    Call<PhotoBarista> uploadImageBarista(@Part("barista") RequestBody barista, @Part MultipartBody.Part file)
 }
