12954e205128aacb7ebc0747f09da8fc5c559b32
Francisco Reyes <francisco@makingdevs.com>
2016-06-27 15:15:47 -0500

    #132 Se agrega firma del metodo para subir foto barista

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/S3assetManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/S3assetManager.groovy
@@ -1,9 +1,11 @@
 package com.makingdevs.mybarista.service
 
 import com.makingdevs.mybarista.model.command.UploadCommand
+import com.makingdevs.mybarista.model.command.UploadPhotoBaristaCommand
 
 interface S3assetManager {
 
     void upload(UploadCommand uploadCommand, Closure onSuccess, Closure onError)
+    void uploadPhotoBarista(UploadPhotoBaristaCommand uploadPhotoBaristaCommand, Closure onSuccess, Closure onError)
 
 }
