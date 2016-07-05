a92d57136073191bad13fc6f02008718afbaa60c
Jorge Acosta Lemus <jorge@makingdevs.com>
2016-07-05 16:38:56 -0500

    #148 agregando método de redimensionamiento y ajuste de imagen

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/CamaraUtil.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/CamaraUtil.groovy
@@ -19,9 +19,9 @@ class CamaraUtil{
     }
 
     Bitmap resizeBitmapFromFilePath(String pathPhoto, Integer width, Integer height){
-        BitmapFactory.Options bmOptions = new BitmapFactory.Options()
-        Bitmap bitmap = BitmapFactory.decodeFile(pathPhoto,bmOptions)
-        bitmap = Bitmap.createScaledBitmap(bitmap,width,height,true)
+        Bitmap bitmap = lessResolution(pathPhoto, width, height)
+        bitmap = Bitmap.createScaledBitmap(bitmap,width,height,false)
+        bitmap
     }
 
     File saveBitmapToFile(Bitmap bitmap,String photoName){
@@ -38,6 +38,44 @@ class CamaraUtil{
             Log.d(TAG,"Error... "+e.message)
         }
         file
+    }
+
+    // NOTE: http://stackoverflow.com/questions/17839388/creating-a-scaled-bitmap-with-createscaledbitmap-in-android
+    private Bitmap lessResolution (String filePath, int width, int height) {
+        int reqHeight = height;
+        int reqWidth = width;
+        BitmapFactory.Options options = new BitmapFactory.Options();
+
+        // First decode with inJustDecodeBounds=true to check dimensions
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(filePath, options);
+
+        // Calculate inSampleSize
+        options.inSampleSize = calculateInSampleSize(options, reqWidth, reqHeight);
+
+        // Decode bitmap with inSampleSize set
+        options.inJustDecodeBounds = false;
+
+        BitmapFactory.decodeFile(filePath, options)
+    }
+
+    private int calculateInSampleSize(BitmapFactory.Options options, int reqWidth, int reqHeight) {
+
+        final int height = options.outHeight;
+        final int width = options.outWidth;
+        int inSampleSize = 1;
+
+        if (height > reqHeight || width > reqWidth) {
+            // Calculate ratios of height and width to requested height and width
+            final int heightRatio = Math.round((float) height / (float) reqHeight) as Integer
+            final int widthRatio = Math.round((float) width / (float) reqWidth) as Integer
+
+            // Choose the smallest ratio as inSampleSize value, this will guarantee
+            // a final image with both dimensions larger than or equal to the
+            // requested height and width.
+            inSampleSize = heightRatio < widthRatio ? heightRatio : widthRatio;
+        }
+        inSampleSize
     }
 
 }
