9d52a0ce76b112c6a03591739977d188be6c5a79
Francisco Reyes <francisco@makingdevs.com>
2016-07-18 17:24:10 -0500

    #157 Se agrega clase para menejar permisos

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/RequestPermissionAndroid.groovy
@@ -0,0 +1,46 @@
+package com.makingdevs.mybarista.common
+
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+
+class RequestPermissionAndroid{
+
+    static final int PERMISSIONS_REQUEST_WRITE = 1
+    static final int PERMISSIONS_REQUEST_CAMERA = 1
+
+    void checkPermission(Activity activity, String permission){
+        switch (permission){
+            case "camera":
+                if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) { }
+                    else {
+                        ActivityCompat.requestPermissions(activity, [Manifest.permission.CAMERA] as String[], PERMISSIONS_REQUEST_CAMERA)
+                    }
+                }
+                break
+            case "storage":
+                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) { }
+                    else {
+                        ActivityCompat.requestPermissions(activity, [Manifest.permission.WRITE_EXTERNAL_STORAGE] as String[], PERMISSIONS_REQUEST_WRITE)
+                    }
+                }
+                break
+            case "location":
+                if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
+                        && ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) { }
+                    else {
+                        ActivityCompat.requestPermissions(activity, [Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION] as String[], PERMISSIONS_REQUEST_WRITE)
+                    }
+                }
+                break
+        }
+
+    }
+
+}
