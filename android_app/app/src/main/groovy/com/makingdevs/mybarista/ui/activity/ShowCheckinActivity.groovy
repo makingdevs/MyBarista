24d1978bc3ed160c4f85f4997c4a616d8ea2513a
Francisco Reyes <francisco@makingdevs.com>
2016-07-19 17:04:57 -0500

    #157 Se agrega implementaicon del metodo para manejar respuesta de permisos

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ShowCheckinActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/ShowCheckinActivity.groovy
@@ -2,16 +2,26 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.support.annotation.NonNull
 import android.support.annotation.Nullable
+import android.support.v4.app.Fragment
 import android.util.Log
+import android.widget.Toast
+import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.common.MultiFragmentActivity
+import com.makingdevs.mybarista.common.RequestPermissionAndroid
+import com.makingdevs.mybarista.model.command.UploadCommand
+import com.makingdevs.mybarista.ui.fragment.CameraFragment
 import com.makingdevs.mybarista.ui.fragment.CommentsFragment
 import com.makingdevs.mybarista.ui.fragment.RatingCoffeFragment
 import com.makingdevs.mybarista.ui.fragment.ShowCheckinFragment
 import com.makingdevs.mybarista.ui.fragment.ShowCircleFlavorFragment
 import groovy.transform.CompileStatic
 import groovy.transform.TypeChecked
+
+import static com.makingdevs.mybarista.common.RequestPermissionAndroid.*
 
 @CompileStatic
 public class ShowCheckinActivity extends MultiFragmentActivity {
@@ -21,22 +31,50 @@ public class ShowCheckinActivity extends MultiFragmentActivity {
     private static final String TAG = "ShowCheckinActivity"
 
 
-    static Intent newIntentWithContext(Context context, String id,String idCircleFlavor){
+    static Intent newIntentWithContext(Context context, String id, String idCircleFlavor) {
         Intent intent = new Intent(context, ShowCheckinActivity)
         intent.putExtra(EXTRA_CHECKIN_ID, id)
-        intent.putExtra(EXTRA_CIRCLE_FLAVOR_ID,idCircleFlavor)
+        intent.putExtra(EXTRA_CIRCLE_FLAVOR_ID, idCircleFlavor)
         intent
     }
 
     @TypeChecked
-    Map createFragments(){
-        if(!getIntent()?.extras.getSerializable(EXTRA_CHECKIN_ID)) throw new IllegalArgumentException("El checkin no tiene ID, mmmm tamales!")
+    Map createFragments() {
+        if (!getIntent()?.extras.getSerializable(EXTRA_CHECKIN_ID)) throw new IllegalArgumentException("El checkin no tiene ID, mmmm tamales!")
         //TODO: cambiar a meotodo newInstance en los frgamentos utilizados
-        [top:new ShowCheckinFragment(), middleTop:new RatingCoffeFragment(), middleBootom: new ShowCircleFlavorFragment(), bottom:new CommentsFragment()]
+        [top: new ShowCheckinFragment(), middleTop: new RatingCoffeFragment(), middleBootom: new ShowCircleFlavorFragment(), bottom: new CommentsFragment()]
     }
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
+    }
+
+    @Override
+    void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+            case PERMISSIONS_REQUEST_WRITE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+            case PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+
+        }
     }
 }
