44069c67f3c1cb457833789e20852d61d77563ba
Francisco Reyes <francisco@makingdevs.com>
2016-07-19 17:01:15 -0500

    #157 Se agrega implementacion del metodo paramanejar respuesta de permiso

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/BaristaActivity.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/activity/BaristaActivity.groovy
@@ -2,10 +2,14 @@ package com.makingdevs.mybarista.ui.activity
 
 import android.content.Context
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.support.v4.app.Fragment
+import android.widget.Toast
 import com.makingdevs.mybarista.common.SingleFragmentActivity
 import com.makingdevs.mybarista.ui.fragment.BaristaFragment
 import groovy.transform.CompileStatic
+
+import static com.makingdevs.mybarista.common.RequestPermissionAndroid.*
 
 @CompileStatic
 class BaristaActivity extends SingleFragmentActivity{
@@ -19,5 +23,33 @@ class BaristaActivity extends SingleFragmentActivity{
     Fragment createFragment() {
         //TODO: cambiar a meotodo newInstance
         new BaristaFragment()
+    }
+
+    @Override
+    void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_CAMERA:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+            case PERMISSIONS_REQUEST_WRITE:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+            case PERMISSIONS_REQUEST_LOCATION:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                } else {
+                    Toast.makeText(this, "Permiso denegado", Toast.LENGTH_SHORT).show()
+                }
+                return;
+                break
+
+        }
     }
 }
