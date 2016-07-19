57d7c7138d324413b274d3646e12193db875f20c
Francisco Reyes <francisco@makingdevs.com>
2016-07-19 10:23:13 -0500

    #157 Se agrega metodo para mostrar mensaje al solicitar de nuevo permisos

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/RequestPermissionAndroid.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/RequestPermissionAndroid.groovy
@@ -2,29 +2,36 @@ package com.makingdevs.mybarista.common
 
 import android.Manifest
 import android.app.Activity
+import android.content.Context
+import android.content.DialogInterface
 import android.content.pm.PackageManager
 import android.support.v4.app.ActivityCompat
 import android.support.v4.content.ContextCompat
+import android.support.v7.app.AlertDialog
+import android.widget.EditText
+import android.widget.Toast
 
-class RequestPermissionAndroid{
+class RequestPermissionAndroid {
 
     static final int PERMISSIONS_REQUEST_WRITE = 1
     static final int PERMISSIONS_REQUEST_CAMERA = 1
 
-    void checkPermission(Activity activity, String permission){
-        switch (permission){
+    void checkPermission(Activity activity, String permission) {
+        switch (permission) {
             case "camera":
                 if (ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) { }
-                    else {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.CAMERA)) {
+                        createPopMenu(activity,"cámara",[Manifest.permission.CAMERA] as String[])
+                    } else {
                         ActivityCompat.requestPermissions(activity, [Manifest.permission.CAMERA] as String[], PERMISSIONS_REQUEST_CAMERA)
                     }
                 }
                 break
             case "storage":
                 if (ContextCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) { }
-                    else {
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+                        createPopMenu(activity,"almacenamiento",[Manifest.permission.WRITE_EXTERNAL_STORAGE] as String[])
+                    } else {
                         ActivityCompat.requestPermissions(activity, [Manifest.permission.WRITE_EXTERNAL_STORAGE] as String[], PERMISSIONS_REQUEST_WRITE)
                     }
                 }
@@ -33,9 +40,10 @@ class RequestPermissionAndroid{
                 if (ContextCompat.checkSelfPermission(activity, Manifest.permission.ACCESS_FINE_LOCATION)
                         && ContextCompat.checkSelfPermission(activity.getApplicationContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
                         != PackageManager.PERMISSION_GRANTED) {
-                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) { }
-                    else {
-                        ActivityCompat.requestPermissions(activity, [Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION] as String[], PERMISSIONS_REQUEST_WRITE)
+                    if (ActivityCompat.shouldShowRequestPermissionRationale(activity, Manifest.permission.ACCESS_FINE_LOCATION)) {
+                        createPopMenu(activity,"localización",[Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION] as String[])
+                    } else {
+                        ActivityCompat.requestPermissions(activity, [Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION] as String[], PERMISSIONS_REQUEST_WRITE)
                     }
                 }
                 break
@@ -43,4 +51,21 @@ class RequestPermissionAndroid{
 
     }
 
+    void createPopMenu(Activity activity,String permission,String[] permissionArray){
+        final int PERMISSION = 1
+        AlertDialog.Builder builder = new AlertDialog.Builder(activity)
+        builder.setMessage("¿Quieres permitir que Barista tenga acceso a ${permission}?")
+                .setPositiveButton("Negar", new DialogInterface.OnClickListener() {
+            void onClick(DialogInterface dialog, int id) {
+                Toast.makeText(activity.getApplicationContext(),"Permiso de ${permission} negado",Toast.LENGTH_SHORT).show()
+            }
+        })
+                .setNegativeButton("Aceptar", new DialogInterface.OnClickListener() {
+            void onClick(DialogInterface dialog, int id) {
+                ActivityCompat.requestPermissions(activity, permissionArray , PERMISSION)
+            }
+        })
+        AlertDialog alertDialog = builder.create()
+        alertDialog.show()
+    }
 }
