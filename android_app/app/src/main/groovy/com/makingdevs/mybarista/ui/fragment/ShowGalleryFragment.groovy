bec86f6f3c0b677515e33a25fc11a5877c16a215
Francisco Reyes <francisco@makingdevs.com>
2016-07-20 18:48:26 -0500

    #159 Se agrega logica para obtener fotos de galeria

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowGalleryFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowGalleryFragment.groovy
@@ -9,19 +9,41 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.makingdevs.mybarista.R
+import com.makingdevs.mybarista.common.ImageUtil
+import com.makingdevs.mybarista.model.PhotoCheckin
+import com.makingdevs.mybarista.ui.adapter.PhotoAdapter
 
-class ShowGalleryFragment extends Fragment{
+class ShowGalleryFragment extends Fragment {
 
-    ShowGalleryFragment(){super()}
+    ShowGalleryFragment() { super() }
     RecyclerView recyclerViewPhotos
+    PhotoAdapter photoAdapter
+    ImageUtil imageUtil = new ImageUtil()
 
     @Override
     View onCreateView(LayoutInflater inflater,
                       @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_show_gallery, container, false)
-//        recyclerViewPhotos = (RecyclerView) root.findViewById(R.id.list_photos)
-//        recyclerViewPhotos.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(),3))
+        recyclerViewPhotos = (RecyclerView) root.findViewById(R.id.list_photos)
+        recyclerViewPhotos.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 3))
         root
+    }
+
+    @Override
+    void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState)
+        photoAdapter = new PhotoAdapter(getActivity(), populateGallery())
+        recyclerViewPhotos.adapter = photoAdapter
+    }
+
+    List<PhotoCheckin> populateGallery() {
+        ArrayList<PhotoCheckin> gallery = new ArrayList<PhotoCheckin>()
+        ArrayList<String> pathPhotos = new ArrayList<String>()
+        pathPhotos = imageUtil.getGalleryPhotos(getActivity())
+        pathPhotos.each {photo->
+            gallery << new PhotoCheckin(url_file:photo)
+        }
+        gallery
     }
 
 }
