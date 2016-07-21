f59472100e2c34b10adf461201cfe51f8522f793
Francisco Reyes <francisco@makingdevs.com>
2016-07-21 15:42:11 -0500

    #159 Se agrega funcion para subir foto

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowGalleryFragment.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/fragment/ShowGalleryFragment.groovy
@@ -1,7 +1,9 @@
 package com.makingdevs.mybarista.ui.fragment
 
+import android.app.Activity
 import android.os.Bundle
 import android.support.annotation.Nullable
+import android.support.design.widget.FloatingActionButton
 import android.support.v4.app.Fragment
 import android.support.v7.widget.GridLayoutManager
 import android.support.v7.widget.RecyclerView
@@ -9,14 +11,17 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.common.ImageUtil
 import com.makingdevs.mybarista.common.OnItemClickListener
+import com.makingdevs.mybarista.common.RequestPermissionAndroid
 import com.makingdevs.mybarista.model.Checkin
 import com.makingdevs.mybarista.model.PhotoCheckin
 import com.makingdevs.mybarista.model.command.UploadCommand
 import com.makingdevs.mybarista.service.S3assetManager
 import com.makingdevs.mybarista.service.S3assetManagerImpl
+import com.makingdevs.mybarista.ui.activity.ShowGalleryActivity
 import com.makingdevs.mybarista.ui.adapter.PhotoAdapter
 import retrofit2.Call
 import retrofit2.Response
@@ -30,6 +35,8 @@ class ShowGalleryFragment extends Fragment implements OnItemClickListener<PhotoC
     String currentUser
     String checkinId
     S3assetManager mS3Manager = S3assetManagerImpl.instance
+    FloatingActionButton floatingActionButtonCamera
+    RequestPermissionAndroid requestPermissionAndroid = new RequestPermissionAndroid()
 
     @Override
     View onCreateView(LayoutInflater inflater,
@@ -37,6 +44,7 @@ class ShowGalleryFragment extends Fragment implements OnItemClickListener<PhotoC
         View root = inflater.inflate(R.layout.fragment_show_gallery, container, false)
         recyclerViewPhotos = (RecyclerView) root.findViewById(R.id.list_photos)
         recyclerViewPhotos.setLayoutManager(new GridLayoutManager(getActivity().getApplicationContext(), 3))
+        floatingActionButtonCamera = (FloatingActionButton) root.findViewById(R.id.button_go_camera)
         root
     }
 
@@ -50,6 +58,29 @@ class ShowGalleryFragment extends Fragment implements OnItemClickListener<PhotoC
         bundle = getArguments()
         currentUser = bundle.getString("USERID")
         checkinId = bundle.getString("CHECKINID")
+
+        floatingActionButtonCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            void onClick(View v) {
+                requestPermissionAndroid.checkPermission(getActivity(), "storage")
+                Fragment cameraFragment = new CameraFragment()
+                cameraFragment.setSuccessActionOnPhoto { File photo ->
+                    uploadPicture(photo)
+                    getActivity().onBackPressed()
+                }
+                cameraFragment.setErrorActionOnPhoto {
+                    Toast.makeText(getContext(), "Error al caputar la foto", Toast.LENGTH_SHORT).show()
+                }
+                getFragmentManager()
+                        .beginTransaction()
+                        .replace(R.id.bottomEmptyFragment, cameraFragment)
+                        .addToBackStack(null).commit()
+            }
+        })
+    }
+
+    void uploadPicture(File photo) {
+        mS3Manager.upload(new UploadCommand(idCheckin: checkinId, idUser: currentUser, pathFile: photo.absolutePath), onSuccessPhoto(), onError())
     }
 
     List<PhotoCheckin> populateGallery() {
@@ -71,8 +102,10 @@ class ShowGalleryFragment extends Fragment implements OnItemClickListener<PhotoC
     private Closure onSuccessPhoto() {
         { Call<PhotoCheckin> call, Response<PhotoCheckin> response ->
             println("Respuesta " + response.body())
-            if (response.body())
+            if (response.body()){
                 getActivity().onBackPressed()
+            }
+
         }
     }
 
