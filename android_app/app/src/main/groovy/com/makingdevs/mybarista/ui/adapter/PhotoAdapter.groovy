9b04be4f759e0b2c68700f86cd592082947bbb16
Francisco Reyes <francisco@makingdevs.com>
2016-07-21 11:44:01 -0500

    #159 Se agrega metodo para escuchar evento de item

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/adapter/PhotoAdapter.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/ui/adapter/PhotoAdapter.groovy
@@ -1,26 +1,26 @@
 package com.makingdevs.mybarista.ui.adapter
 
 import android.content.Context
-import android.content.Intent
 import android.support.v7.widget.RecyclerView
-import android.text.format.DateUtils
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
-import android.widget.RatingBar
-import android.widget.TextView
 import com.makingdevs.mybarista.R
 import com.makingdevs.mybarista.common.ImageUtil
-import com.makingdevs.mybarista.model.Checkin
+import com.makingdevs.mybarista.common.OnItemClickListener
 import com.makingdevs.mybarista.model.PhotoCheckin
-import com.makingdevs.mybarista.ui.activity.ShowCheckinActivity
+import com.makingdevs.mybarista.service.S3assetManager
+import com.makingdevs.mybarista.service.S3assetManagerImpl
 
 class PhotoAdapter extends RecyclerView.Adapter<PhotoViewHolder> {
 
     Context mContext
     List<PhotoCheckin> mPhotos
     ImageUtil mImageUtil1 = new ImageUtil()
+    S3assetManager mS3Manager = S3assetManagerImpl.instance
+    OnItemClickListener<PhotoCheckin> onItemClickListener
+
 
     PhotoAdapter(Context context, List<PhotoCheckin> photoList) {
         mContext = context
@@ -44,34 +44,28 @@ class PhotoAdapter extends RecyclerView.Adapter<PhotoViewHolder> {
         mPhotos.size()
     }
 
+    void setOnClickListener(OnItemClickListener<PhotoCheckin> onItemClickListener) {
+        this.onItemClickListener = onItemClickListener
+    }
 
-    class PhotoViewHolder extends RecyclerView.ViewHolder {
+    class PhotoViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
 
         ImageView photoImageView
 
         void bindCheckin(PhotoCheckin photo) {
-            /*mCheckin = checkin
-            itemView.onClickListener = {
-                Intent intent = ShowCheckinActivity.newIntentWithContext(mContext,mCheckin.id,mCheckin.circle_flavor_id)
-                mContext.startActivity(intent)
-            }
-            mtitle.text = checkin.method
-            morigin.text = checkin.origin
-            String fecha = DateUtils.getRelativeTimeSpanString(mCheckin.created_at.time, new Date().time, 0).toString()
-            mmoment.text = fecha
-            mprice.text = "${checkin.price ? "\$ ${checkin.price}" : ""}"
-            mrating.setRating(Float.parseFloat(checkin.rating ?: "0"))
-            if(mCheckin.s3_asset?.url_file)
-                mImageUtil1.setPhotoImageView(mContext, mCheckin.s3_asset?.url_file, photoImageView)
-            else
-                photoImageView.setImageResource(R.drawable.coffee)*/
-
+            mImageUtil1.setPhotoImageView(mContext, photo.url_file, photoImageView)
         }
 
         PhotoViewHolder(View itemView) {
             super(itemView)
             photoImageView = (ImageView) itemView.findViewById(R.id.photo_gallery)
+            itemView.setOnClickListener(this)
+        }
 
+        @Override
+        void onClick(View v) {
+            if (onItemClickListener)
+                onItemClickListener.onItemClicked(mPhotos.get(getAdapterPosition()))
         }
     }
 
