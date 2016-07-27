f1488bf9e6c3d02e00f08de9fa45125d9603a24c
Francisco Reyes <francisco@makingdevs.com>
2016-07-27 17:23:57 -0500

    #169 Se ajusta implementacion para mostrar dialogos

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/view/LoadingDialog.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/view/LoadingDialog.groovy
@@ -1,23 +1,30 @@
 package com.makingdevs.mybarista.view
 
-import android.R
 import android.app.AlertDialog
 import android.app.Dialog
-import android.app.DialogFragment
 import android.os.Bundle
+import android.support.annotation.StringRes
+import android.support.v4.app.DialogFragment
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
+import com.makingdevs.mybarista.R
+import groovy.transform.CompileStatic
 
+@CompileStatic
 class LoadingDialog extends DialogFragment {
 
-    static final String TITLE = "title"
-    static final String TITLE_RES_ID = "title_res_id"
+    LoadingDialog() {}
 
-    LoadingDialog newInstance(String title) {
+    static final String MSG_RES_ID = "title_res_id"
+    TextView labelLoading
+
+    static LoadingDialog newInstance(@StringRes int message) {
         Bundle args = new Bundle()
-        args.putString(TITLE, title)
+        args.putInt(MSG_RES_ID, message)
 
         LoadingDialog loadingDialog = new LoadingDialog()
+        loadingDialog.setCancelable(false)
         loadingDialog.arguments = args
 
         loadingDialog
@@ -25,9 +32,11 @@ class LoadingDialog extends DialogFragment {
 
     @Override
     Dialog onCreateDialog(Bundle savedInstanceState) {
-        return super.onCreateDialog(savedInstanceState)
 
-        View view = LayoutInflater.from(context).inflate(R.layout.dialog_loading, null)
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_loading_data, null)
+
+        labelLoading = (TextView) view.findViewById(R.id.label_loading)
+        labelLoading.text = getMessage()
 
         AlertDialog.Builder builder = new AlertDialog.Builder(context)
         builder.setView(view)
@@ -35,7 +44,7 @@ class LoadingDialog extends DialogFragment {
         builder.create()
     }
 
-    CharSequence getTitle() {
-        arguments.getString(TITLE)
+    String getMessage() {
+        getString(arguments.getInt(MSG_RES_ID))
     }
 }
