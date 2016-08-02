285072ef5ac0618b24ad4f418f37e2af63c69c7b
Ariana Santillán <arianarsdev@gmail.com>
2016-08-02 17:55:44 -0500

    #175 Se creó el dialog fragment para agregar una nota al check in

---
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/view/NoteDialog.groovy
@@ -0,0 +1,64 @@
+package com.makingdevs.mybarista.view
+
+import android.app.Dialog
+import android.content.DialogInterface
+import android.os.Bundle
+import android.support.v4.app.DialogFragment
+import android.support.v7.app.AlertDialog
+import android.view.LayoutInflater
+import android.view.View
+import android.widget.EditText
+import com.makingdevs.mybarista.R
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class NoteDialog extends DialogFragment {
+
+    NoteDialog() {}
+
+    static final String NOTE = "message_res_id"
+    EditText previewNote
+
+    static NoteDialog newInstance(String message) {
+        Bundle args = new Bundle()
+        args.putString(NOTE, message)
+
+        NoteDialog noteDialog = new NoteDialog()
+        noteDialog.setCancelable(false)
+        noteDialog.arguments = args
+
+        noteDialog
+    }
+
+    @Override
+    Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        View view = LayoutInflater.from(context).inflate(R.layout.dialog_note, null)
+
+        previewNote = (EditText) view.findViewById(R.id.previewNote)
+        previewNote.text = getMessage()
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(context)
+        builder.setView(view)
+
+        builder.setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+            @Override
+            void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss()
+            }
+        })
+
+        builder.setNegativeButton("Cancelar", new DialogInterface.OnClickListener() {
+            @Override
+            void onClick(DialogInterface dialog, int which) {
+                dialog.dismiss()
+            }
+        })
+
+        builder.create()
+    }
+
+    String getMessage() {
+        arguments.getString(NOTE)
+    }
+}
