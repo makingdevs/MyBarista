e0e52437beb995fdbbca316584301ac683aa971b
Ariana Santillán <arianarsdev@gmail.com>
2016-08-03 16:04:40 -0500

    #178 Se implementa método para guardar nota

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckingManagerImpl.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckingManagerImpl.groovy
@@ -1,11 +1,9 @@
 package com.makingdevs.mybarista.service
 
-import android.util.Log
 import com.makingdevs.mybarista.model.command.CheckinCommand
 import com.makingdevs.mybarista.model.command.CircleFlavorCommand
 import com.makingdevs.mybarista.network.CheckinRestOperations
 import com.makingdevs.mybarista.network.impl.RetrofitTemplate
-import com.makingdevs.mybarista.ui.fragment.CircleFlavorFragment
 import groovy.transform.CompileStatic
 
 @Singleton
@@ -24,36 +22,43 @@ class CheckingManagerImpl implements CheckinManager {
 
     @Override
     void list(Map params, Closure onSuccess, Closure onError) {
-        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ CheckinRestOperations restOperations ->
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError) { CheckinRestOperations restOperations ->
             restOperations.getCheckins(params)
         }
     }
 
     @Override
-    void show(String id, Closure onSuccess, Closure onError){
-        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ CheckinRestOperations restOperations ->
+    void show(String id, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError) { CheckinRestOperations restOperations ->
             restOperations.getCheckin(id)
         }
     }
 
     @Override
-    void saveCircle(String id,CircleFlavorCommand circleFlavorCommand, Closure onSuccess, Closure onError) {
-        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ CheckinRestOperations restOperations ->
-            restOperations.createCircleFlavor(id,circleFlavorCommand)
+    void saveCircle(String id, CircleFlavorCommand circleFlavorCommand, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError) { CheckinRestOperations restOperations ->
+            restOperations.createCircleFlavor(id, circleFlavorCommand)
         }
     }
 
     @Override
-    void showCircleFlavor(String id, Closure onSuccess, Closure onError){
-        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ CheckinRestOperations restOperations ->
+    void showCircleFlavor(String id, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError) { CheckinRestOperations restOperations ->
             restOperations.getCircleFlavor(id)
         }
     }
 
     @Override
-    void  saveRating(String id, CheckinCommand command, Closure onSuccess,Closure onError){
-        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError){ CheckinRestOperations restOperations ->
-            restOperations.setRationgCoffee(id,command)
+    void saveRating(String id, CheckinCommand command, Closure onSuccess, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSuccess, onError) { CheckinRestOperations restOperations ->
+            restOperations.setRationgCoffee(id, command)
+        }
+    }
+
+    @Override
+    void saveNote(String id, CheckinCommand checkinCommand, Closure onSucces, Closure onError) {
+        RetrofitTemplate.instance.withRetrofit(operations as Class, onSucces, onError) { CheckinRestOperations restOperations ->
+            restOperations.setNoteInCheckin(id, checkinCommand)
         }
     }
 }
