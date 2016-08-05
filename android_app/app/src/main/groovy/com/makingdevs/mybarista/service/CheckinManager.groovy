644fc7117e186deb1291967824f8ecaa7e0e696c
Ariana Santillán <arianarsdev@gmail.com>
2016-08-05 16:05:02 -0500

    #179 Se creó método para actualizar Checkin

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/service/CheckinManager.groovy
@@ -9,6 +9,7 @@ interface CheckinManager {
     void save(CheckinCommand checkin, Closure onSuccess, Closure onError)
     void list(Map params, Closure onSuccess, Closure onError)
     void show(String id, Closure onSuccess, Closure onError)
+    void update(String id, CheckinCommand checkinCommand, Closure onSuccess, Closure onError)
     void saveCircle(String id,CircleFlavorCommand circleFlavorCommand,Closure onSuccess, Closure onError)
     void showCircleFlavor(String id, Closure onSuccess, Closure onError)
     void saveRating(String id, CheckinCommand checkinCommand, Closure onSuccess,Closure onError)
