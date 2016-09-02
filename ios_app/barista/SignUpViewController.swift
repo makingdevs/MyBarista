add47bdd643241c9819cc9ddc9d16a2a09b46279
Olga <mora31.olga@gmail.com>
2016-09-02 12:04:54 -0500

    #214 Se agrega muestra de request

--- ios_app/barista/SignUpViewController.swift
+++ ios_app/barista/SignUpViewController.swift
@@ -6,4 +6,29 @@
 //  Copyright © 2016 MakingDevs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import Alamofire
+import SwiftyJSON
+
+class SignUpViewController: UIViewController {
+    
+    @IBAction func sendRequest(sender: UIButton) {
+        // let parameterzs  = [:]
+        // Alamofire.request(.POST, "http://mybarista.makingdevs.com/user/neodevelop", parameters: parameters)
+        Alamofire.request(.GET, "http://mybarista.makingdevs.com/user/neodevelop")
+            .responseJSON { response in
+            if let value = response.result.value{
+                let json = JSON(value)
+                print(json)
+                let checkins = json["checkins"]
+                for(_, subJson) in checkins {
+                    print("***")
+                    let id = subJson["id"].stringValue
+                    print(id)
+                    let author = subJson["author"].stringValue
+                    print(author)
+                }
+            }
+        }
+    }
+}
