6686ef3dbfd48093943b49fc02ddf98a5336f94e
Ariana Santillán <arianarsdev@gmail.com>
2016-09-05 14:49:05 -0500

    #207 Se importó el módulo Users.Commands para responer a las tareas del usuario

--- web_app/src/Main.elm
+++ web_app/src/Main.elm
@@ -1,12 +1,12 @@
 
 module Main exposing (..)
 
--- Elm Core
 import Models exposing (..)
 import Messages exposing (Msg(..))
 import Update exposing (update)
 import View exposing (view)
 import Routing exposing (..)
+import Users.Commands exposing (..)
 import Http
 import Task exposing (Task)
 import Json.Decode as Decode exposing ((:=))
@@ -22,61 +22,6 @@ init result =
                          , checkins = []
                          , checkins_count = 0 }
                        )
-
--- Base url
-api : String
-api =
-    "http://mybarista.makingdevs.com/"
-
--- User endpoint
-userUrl : String
-userUrl =
-    api ++ "user/"
-
--- User command
-fetchUserCmd : String -> Cmd Msg
-fetchUserCmd username =
-    Http.get userDecoder (userUrl ++ username)
-        |> Task.perform FetchUserError FetchUserSuccess
-
--- User decoder
-userDecoder : Decode.Decoder Model
-userDecoder =
-    Decode.object6 Model
-        ("id" := Decode.int)
-        (Decode.maybe("name" := Decode.string))
-        ("username" := Decode.string)
-        (Decode.maybe("s3_asset" := s3AssetDecoder))
-        ("checkins" := checkinsDecoder)
-        ("checkins_count" := Decode.int)
-
--- Checkin S3Asset decoder
-checkinAssetDecoder : Decode.Decoder CheckinS3Asset
-checkinAssetDecoder =
-    Decode.object2 CheckinS3Asset
-        ("id" := Decode.int)
-        ("url_file" := Decode.string)
-
--- S3Asset decoder
-s3AssetDecoder : Decode.Decoder S3Asset
-s3AssetDecoder =
-    Decode.object1 S3Asset
-        ("url_file" := Decode.string)
-
--- Checkin and List Checkin decoder
-checkinsDecoder : Decode.Decoder (List Checkin)
-checkinsDecoder =
-    Decode.list checkinDecoder
-
-checkinDecoder : Decode.Decoder Checkin
-checkinDecoder =
-    Decode.object3 Checkin
-        ("author" := Decode.string)
-        ("id" := Decode.int)
-        (Decode.maybe("s3_asset" := checkinAssetDecoder))
-
-
--- SUBSCRIPTIONS
 
 
 subscriptions : Model -> Sub Msg
