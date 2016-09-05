77fdfa412da4804d1a29ce6857887410b71f8d9c
Ariana Santillán <arianarsdev@gmail.com>
2016-09-05 13:04:00 -0500

    #207 Se importó el módulo View para renderear HTML

--- web_app/src/Main.elm
+++ web_app/src/Main.elm
@@ -5,9 +5,7 @@ module Main exposing (..)
 import Models exposing (..)
 import Messages exposing (Msg(..))
 import Update exposing (update)
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import View exposing (view)
 import Http
 import Task exposing (Task)
 import Json.Decode as Decode exposing ((:=))
@@ -63,7 +61,6 @@ init result =
 
 -- UPDATE
 
-
 -- Base url
 api : String
 api =
@@ -116,120 +113,6 @@ checkinDecoder =
         ("id" := Decode.int)
         (Decode.maybe("s3_asset" := checkinAssetDecoder))
 
-
--- CHILD VIEWS
-
--- Navigation
-navigation : Html.Html Msg
-navigation =
-    div[ class "navigation-main row" ]
-        [ div [ class "navigation__brand col-xs-6 col-sm-6 col-md-6" ]
-              [ div [ class "navigation__brand-container" ]
-                    [ img [ src "http://barist.coffee.s3.amazonaws.com/ic_barista_logo.png", class "navigation__image"] []
-                    ]
-              , div [ class "navigation__text col-sm-4 col-md-4" ]
-                    [ h1 []
-                          [ text "Barista"]
-                    ]
-              ]
-        , div [ class "navigation__href-session col-xs-6 col-sm-4 col-md-4" ]
-              [ ul [ class "navigation__session-items"]
-                    [ li [ class "navigation__session-item"] [ a [href "https://play.google.com/store/apps/details?id=makingdevs.com.mybarista"] [text "Descarga la applicación"] ]
-                    ]
-              ]
-        ]
-
--- Profile
-profile : Model -> Html.Html Msg
-profile model =
-    div [ class "profile-page row" ]
-        [ div [ class "profile-page__header"]
-              [ div [ class "profile-page__author-container row" ]
-                    [ div [ class "profile-page__avatar-container col-xs-4 col-sm-4 col-md-4" ]
-                          [ img [ src (model.s3_asset |> Maybe.map .url_file |> Maybe.withDefault "http://barist.coffee.s3.amazonaws.com/avatar.png"), class "profile-page__avatar img-circle" ] []
-                          ]
-                    , div [ class "profile-page__user-info col-xs-8 col-sm-8 col-md-8" ]
-                          [ div [ class "profile-page__username" ]
-                                [ p []
-                                      [ text model.username ]
-                                ]
-                          , div [ class "profile-page__statistics" ]
-                                [ p []
-                                      [ text ( (toString model.checkins_count) ++ " checkins") ]
-                                ]
-                          ]
-                    ]
-              ]
-        ]
-
--- Grid
-renderCheckin : Checkin -> Html.Html Msg
-renderCheckin checkin =
-  li [ class "post-grid__item"] [ img [ class "post-grid__item-image", src <| (checkin.s3_asset |> Maybe.map .url_file |> Maybe.withDefault "http://barist.coffee.s3.amazonaws.com/coffee.jpg")] [] ]
-
-renderCheckins : List Checkin -> Html.Html Msg
-renderCheckins checkins =
-  let
-    items = List.map renderCheckin checkins
-  in
-    ul [ class "post-grid__items"] items
-
-grid : Model -> Html.Html Msg
-grid model =
-    div [ class "post-grid" ]
-        [ div [ class "post-grid__items-container"]
-              [ renderCheckins model.checkins ]
-        ]
-
--- Footer
-baristUrl : String
-baristUrl =
-    "http://www.barist.coffee/"
-storeUrl : String
-storeUrl =
-    "https://play.google.com/store/apps/details?id=makingdevs.com.mybarista"
-devsUrl : String
-devsUrl =
-    "http://makingdevs.com"
-
-footer : Html.Html Msg
-footer =
-    div [ class "footer-main" ]
-        [ div [ class "footer-nav" ]
-              [ ul [ class "footer__nav-items"]
-                    [ li [ class "footer__nav-item" ]
-                         [ a [ href baristUrl ]
-                             [ text "Barista" ]
-                         ]
-                    , li [ class "footer__nav-item" ]
-                         [ a [ href storeUrl ]
-                             [ text "Google Play" ]
-                         ]
-                    , li [ class "footer__nav-item col" ]
-                         [ a [ href devsUrl ]
-                             [ text "Making Devs"]
-                         ]
-                    ]
-              ]
-        ]
-
--- VIEW
-
-
-view : Model -> Html Msg
-view model =
-  div [ class "shell" ]
-      [ div [class "shell__nav"]
-            [ navigation
-            ]
-      , div [class "shell__content"]
-            [ profile model
-            , grid model
-            ]
-      , div [class "shell__footer"]
-            [ footer
-            ]
-      ]
 
 -- SUBSCRIPTIONS
 
