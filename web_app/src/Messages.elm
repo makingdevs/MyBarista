1df8a17aff8b7e13b3f19120cf51b848ee4e3ca0
Ariana Santillán <arianarsdev@gmail.com>
2016-09-06 12:12:23 -0500

    #210 Se agregaron los msgs para mostrar/cancelar el modal dialog de cada Check In

--- web_app/src/Messages.elm
+++ web_app/src/Messages.elm
@@ -1,9 +1,11 @@
 module Messages exposing (..)
 
-import Models exposing (Model)
+import Models exposing (Model, Checkin)
 import Http
 
 
 type Msg
   = FetchUserSuccess Model
     | FetchUserError Http.Error
+    | ShowCheckinDialog Checkin
+    | CancelCheckinDialog
