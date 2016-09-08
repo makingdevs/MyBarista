c7d71a32a201f3cf340d202201cfc62f165a93b5
Ariana Santillán <arianarsdev@gmail.com>
2016-09-08 09:57:40 -0500

    #210 Se actualizó el campo show_checkin en el modelo User de acuerdo a la acción del usuario

--- web_app/src/Update.elm
+++ web_app/src/Update.elm
@@ -25,9 +25,37 @@ update msg model =
     ShowCheckinDialog checkin ->
         Debug.log ("Checkin clicked! " ++ ( toString checkin.id ) )
             ( { model
-                  | show_checkin = Just True }
+                  | show_checkin = Just True
+                  , checkins =
+                    [ { author = checkin.author
+                      , id = checkin.id
+                      , s3_asset = Just { id = checkin.s3_asset
+                                               |> Maybe.map .id
+                                               |> Maybe.withDefault 0
+                                        , url_file =  checkin.s3_asset
+                                                      |> Maybe.map .url_file
+                                                      |> Maybe.withDefault ""
+                                        }
+                      , show_checkin = Just True
+                      }
+                    ]
+              }
             , Cmd.none )
-    CancelCheckinDialog ->
+    CancelCheckinDialog checkin ->
        ( { model
-             | show_checkin = Just False }
+             | show_checkin = Just False
+             , checkins =
+               [ { author = checkin.author
+                 , id = checkin.id
+                 , s3_asset = Just { id = checkin.s3_asset
+                                          |> Maybe.map .id
+                                          |> Maybe.withDefault 0
+                                   , url_file = checkin.s3_asset
+                                                |> Maybe.map .url_file
+                                                |> Maybe.withDefault ""
+                                   }
+                 , show_checkin = Just False
+                 }
+               ]
+         }
        , Cmd.none )
