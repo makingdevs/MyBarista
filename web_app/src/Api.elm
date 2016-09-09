4eb3c0ab59cb3e65077734fd6441e62f18e0ece7
Ariana Santillán <arianarsdev@gmail.com>
2016-09-09 12:05:27 -0500

    #226 Se creó una función para el endpoint de comentarios de Check In

--- web_app/src/Api.elm
+++ web_app/src/Api.elm
@@ -2,10 +2,14 @@
 module Api exposing (..)
 
 
-api : String
-api =
+baseUrl : String
+baseUrl =
     "http://mybarista.makingdevs.com/"
 
-user : String
-user =
-    api ++ "user/"
+userUrl : String -> String
+userUrl username =
+    baseUrl ++ "user/" ++ username
+
+commentsUrl : Int -> String
+commentsUrl id =
+    baseUrl ++ "checkins/" ++ (toString id) ++ "/comments"
