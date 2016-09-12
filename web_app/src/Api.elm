d14ada51f5af3620be779315c4f2f8e55197f225
Ariana Santillán <arianarsdev@gmail.com>
2016-09-12 12:21:11 -0500

    #228 Se agregó el enpoint para HTTP GET request de Checkin

--- web_app/src/Api.elm
+++ web_app/src/Api.elm
@@ -4,12 +4,12 @@ module Api exposing (..)
 
 baseUrl : String
 baseUrl =
-    "http://mybarista.makingdevs.com/"
+    "http://192.168.1.244:3000/"
 
 userUrl : String -> String
 userUrl username =
     baseUrl ++ "user/" ++ username
 
-commentsUrl : Int -> String
-commentsUrl id =
-    baseUrl ++ "checkins/" ++ (toString id) ++ "/comments"
+checkinUrl : Int -> String
+checkinUrl id =
+    baseUrl ++ "checkins/" ++ (toString id)
