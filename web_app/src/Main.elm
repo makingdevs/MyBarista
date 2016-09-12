17b23c2b26d19a704aaf8aff1e198247504d549c
Ariana Santillán <arianarsdev@gmail.com>
2016-09-12 11:46:40 -0500

    #228 Se inicializó la lista de comentarios dentro de la función init

--- web_app/src/Main.elm
+++ web_app/src/Main.elm
@@ -24,7 +24,7 @@ init result =
                                           , s3_asset = Just { id = 0
                                                             , url_file = ""
                                                             }
-                                          , comments = Just []
+                                          , comments = []
                                           , show_checkin = Just False
                                           }
                                         ]
