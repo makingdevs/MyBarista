98cb4a6fc01c130f89db95310bfba4adb4c73449
Ariana Santillán <arianarsdev@gmail.com>
2016-09-23 11:30:11 -0500

    #240 Se agregó el callback manager para dirigir el estatus de la publicación al método onCreate()

--- android_app/app/src/main/groovy/com/makingdevs/mybarista/common/OnActivityResultGallery.groovy
+++ android_app/app/src/main/groovy/com/makingdevs/mybarista/common/OnActivityResultGallery.groovy
@@ -4,6 +4,7 @@ import android.app.Activity
 import android.content.Context
 import android.content.Intent
 import android.widget.ImageView
+import com.facebook.CallbackManager
 import groovy.transform.CompileStatic
 
 @CompileStatic
@@ -11,8 +12,12 @@ trait OnActivityResultGallery {
     ImageView showImage
     ImageUtil mImageUtil1 = new ImageUtil()
     String pathPhoto
+    CallbackManager callbackManager
 
     void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        callbackManager.onActivityResult(requestCode, resultCode, data)
+
         if (requestCode == 1) {
             if (resultCode == Activity.RESULT_OK) {
                 pathPhoto = data.getStringExtra("PATH_PHOTO")
