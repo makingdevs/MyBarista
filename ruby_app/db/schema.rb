588b00ff9e68358ccee0d43e98121efbb47d18ed
Ariana Santillán <arianarsdev@gmail.com>
2016-10-11 17:08:30 -0500

    #195 Cambió la estructura de la DB tras correr la migración

--- ruby_app/db/schema.rb
+++ ruby_app/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011154310) do
+ActiveRecord::Schema.define(version: 20161011213938) do
 
   create_table "barista", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string   "name"
