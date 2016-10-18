05bb77ea01cc38c471f4e0cca3bd0e523830f2fc
Ariana Gothwski <arianarsdev@gmail.com>
2016-10-18 17:38:22 -0500

    #215 This file changed its syntax to Alamofire 4

--- ios_app/barista/CheckinManager.swift
+++ ios_app/barista/CheckinManager.swift
@@ -12,11 +12,11 @@ import SwiftyJSON
 
 class CheckinManager {
     
-    static func findAllCheckinsByUser(username: String, onSuccess:(checkins:[Checkin]) -> (), onError:(error:String) -> () ){
+    static func findAllCheckinsByUser(_ username: String, onSuccess:@escaping (_ checkins:[Checkin]) -> (), onError:@escaping (_ error:String) -> () ){
         let parameters = ["username":username]
-        Alamofire.request(.GET, "http://mybarista.makingdevs.com/checkins", parameters: parameters).responseJSON{ response in
+        Alamofire.request("http://mybarista.makingdevs.com/checkins/", parameters: parameters).responseJSON{ response in
             switch response.result {
-            case .Success:
+            case .success:
                 var checkins = [Checkin]()
                 if let value = response.result.value {
                     let json = JSON(value)
@@ -31,9 +31,9 @@ class CheckinManager {
                         checkins.append(checkin)
                     }
                 }
-                onSuccess(checkins: checkins)
-            case .Failure(let error):
-                onError(error: error.description)
+                onSuccess(checkins)
+            case .failure(let error):
+                onError(error.localizedDescription)
             }
         }
     }
