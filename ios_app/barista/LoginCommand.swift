849a91feb8763e37a7b19422f5070ab8abe82a9b
Ariana Gothwski <arianarsdev@gmail.com>
2016-10-18 17:32:14 -0500

    #215 The syntax was updated to Swift 3

--- ios_app/barista/LoginCommand.swift
+++ ios_app/barista/LoginCommand.swift
@@ -22,6 +22,6 @@ class LoginCommand {
     func validateCommand() -> Bool {
         let regex = "[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[A-Za-z]{2,4}"
         let username = NSPredicate(format:"SELF MATCHES %@", regex)
-        return username.evaluateWithObject(self.username)
+        return username.evaluate(with: self.username)
     }
 }
