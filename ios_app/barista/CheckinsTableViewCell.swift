e4c10d7458b86771f6073761a2c4667a3df36c76
Ariana Santillán <arianarsdev@gmail.com>
2016-10-20 11:41:36 -0500

    #273 CheckinsTableViewCell was added to the project structure

---
+++ ios_app/barista/CheckinsTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CheckinsTableViewCell.swift
+//  barista
+//
+//  Created by Ariana Santillán on 20/10/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class CheckinsTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
