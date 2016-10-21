5eac4ec66289faad18ae90acef8847d569fc7e90
Ariana Santillán <arianarsdev@gmail.com>
2016-10-21 12:19:35 -0500

    #273 UIView class was extended to show the rating coffee

--- ios_app/barista/ViewExtensions.swift
+++ ios_app/barista/ViewExtensions.swift
@@ -2,8 +2,23 @@
 //  ViewExtensions.swift
 //  barista
 //
-//  Created by Ariana Santillán on 21/10/16.
+//  Created by Ariana Gothwski on 21/10/16.
 //  Copyright © 2016 MakingDevs. All rights reserved.
 //
 
-import Foundation
+import UIKit
+import FDRatingView
+
+extension UIView {
+    
+    func loadRating (rating: Float) {
+        let ratingView = FDRatingView(frame: CGRect(x: 32, y: 32, width: 16, height: 16),
+                                      style:.star,
+                                      numberOfElements: 5,
+                                      fillValue: rating,
+                                      color: UIColor.brown,
+                                      lineWidth:0.7,
+                                      spacing:3.0)
+        self.addSubview(ratingView)
+    }
+}
