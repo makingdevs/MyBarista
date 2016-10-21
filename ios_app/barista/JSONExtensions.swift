10fe73ad5234cd75343612d0179a1ac4fd174ca0
Ariana Santillán <arianarsdev@gmail.com>
2016-10-21 18:43:13 -0500

    #273 SwiftyJSON library was extended to parse date values

--- ios_app/barista/JSONExtensions.swift
+++ ios_app/barista/JSONExtensions.swift
@@ -7,3 +7,30 @@
 //
 
 import Foundation
+import SwiftyJSON
+
+extension JSON {
+    
+    public var dateValue: NSDate? {
+        get {
+            switch self.type {
+            case .string:
+                return Formatter.jsonDateFormatter.date(from: self.object as! String) as NSDate?
+
+            default:
+                return nil
+            }
+        }
+    }
+    
+    public var timeValue: NSDate? {
+        get {
+            switch self.type {
+            case .string:
+                return Formatter.jsonDateTimeFormatter.date(from: self.object as! String) as NSDate?
+            default:
+                return nil
+            }
+        }
+    }
+}
