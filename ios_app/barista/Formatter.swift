c599f9b7a2c6b7b3b9bec67a389849891edba5d2
Ariana Santillán <arianarsdev@gmail.com>
2016-10-21 18:45:42 -0500

    #273 This class was created to format dates from JSON

--- ios_app/barista/Formatter.swift
+++ ios_app/barista/Formatter.swift
@@ -7,3 +7,26 @@
 //
 
 import Foundation
+
+class Formatter {
+    
+    private static var internalJsonDateFormatter: DateFormatter?
+    private static var internalJsonDateTimeFormatter: DateFormatter?
+    
+    static var jsonDateFormatter: DateFormatter {
+        if (internalJsonDateFormatter == nil) {
+            internalJsonDateFormatter = DateFormatter()
+            internalJsonDateFormatter!.dateFormat = "yyyy-MM-dd"
+        }
+        return internalJsonDateFormatter!
+    }
+    
+    static var jsonDateTimeFormatter: DateFormatter {
+        if (internalJsonDateTimeFormatter == nil) {
+            internalJsonDateTimeFormatter = DateFormatter()
+            internalJsonDateTimeFormatter!.dateFormat = "yyyy'-'MM'-'dd'T'HH':'mm':'ss.SSS'Z'"
+        }
+        return internalJsonDateTimeFormatter!
+    }
+
+}
