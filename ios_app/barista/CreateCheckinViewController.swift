d3debfe1c808fc52054c3cca320c6e6ab25966ca
Ariana Santillán <arianarsdev@gmail.com>
2016-10-28 11:52:57 -0500

    #275 CreateCheckin controller was added to the project

---
+++ ios_app/barista/CreateCheckinViewController.swift
@@ -0,0 +1,18 @@
+//
+//  CreateCheckinViewController.swift
+//  barista
+//
+//  Created by Ariana Santillán on 28/10/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class CreateCheckinViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
