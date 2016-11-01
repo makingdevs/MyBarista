65c387a79f5b7c32b3837ae5bdc95452746a19b1
Ariana Santillán <arianarsdev@gmail.com>
2016-11-01 15:51:35 -0600

    #281 S3AssetManager was added to the project to upload pictures

---
+++ ios_app/barista/S3AssetManager.swift
@@ -0,0 +1,13 @@
+//
+//  S3AssetManager.swift
+//  barista
+//
+//  Created by Ariana Santillán on 01/11/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import Foundation
+
+class S3AssetManager {
+    
+}
