f37c92405b0a360aa2c677f53ba285ffaf1c3e17
Ariana Santillán <arianarsdev@gmail.com>
2016-11-03 10:17:08 -0600

    #281 Function to upload check-in photo using multipart/from-data was added

--- ios_app/barista/S3AssetManager.swift
+++ ios_app/barista/S3AssetManager.swift
@@ -2,7 +2,7 @@
 //  S3AssetManager.swift
 //  barista
 //
-//  Created by Ariana Santillán on 01/11/16.
+//  Created by MakingDevs on 01/11/16.
 //  Copyright © 2016 MakingDevs. All rights reserved.
 //
 
@@ -14,8 +14,27 @@ class S3AssetManager {
     static func upload(uploadCommand: UploadCommand, onPhotoSuccess: @escaping (_ photoCheckin: PhotoCheckin) -> (), onError: @escaping (_ error: String) -> () ) {
         
         let checkinPhotoURL: String = "http://mybarista.makingdevs.com/checkin/photo/save"
-        let parameters = [
-            "url_file": uploadCommand.path
-        ]
+        
+        Alamofire.upload(
+            multipartFormData: { multipartFromData in
+                multipartFromData.append(
+                    UIImagePNGRepresentation(uploadCommand.image)!,
+                    withName: uploadCommand.imageName,
+                    fileName: uploadCommand.imageFileName,
+                    mimeType: uploadCommand.imageMimeType
+                )
+            },
+            to: checkinPhotoURL,
+            encodingCompletion: { encodingResult in
+                switch encodingResult {
+                case .success(let upload, _, _):
+                    upload.responseJSON {
+                        response in
+                        print(response)
+                    }
+                case .failure(let encodingError):
+                    print(encodingError)
+                }
+        })
     }
 }
