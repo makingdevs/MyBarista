8276ba6abbef7b9cfcc104900dad890ed84af721
Ariana Santillán <arianarsdev@gmail.com>
2016-11-03 10:18:47 -0600

    #281 Parameters for multipart/from-data in pictures were added

--- ios_app/barista/UploadCommand.swift
+++ ios_app/barista/UploadCommand.swift
@@ -2,21 +2,30 @@
 //  UploadCommand.swift
 //  barista
 //
-//  Created by Ariana Santillán on 01/11/16.
+//  Created by MakingDevs on 01/11/16.
 //  Copyright © 2016 MakingDevs. All rights reserved.
 //
 
 import Foundation
+import UIKit
 
 class UploadCommand {
     
     var userId: String
     var checkinId: String
-    var path: String
+    var image: UIImage
+    var imagePath: String
+    var imageName: String
+    var imageFileName: String
+    var imageMimeType: String
     
-    init(userId: String, checkinId: String, path: String) {
+    init(userId: String, checkinId: String, image: UIImage, imagePath: String, imageName: String, imageFileName: String, imageMimeType: String) {
         self.userId = userId
         self.checkinId = checkinId
-        self.path = path
+        self.image = image
+        self.imagePath = imagePath
+        self.imageName = imageName
+        self.imageFileName = imageFileName
+        self.imageMimeType = imageMimeType
     }
 }
