8d1c734c1a65ac52409237f90bd46daefdcbe7ce
Ariana Santillán <arianarsdev@gmail.com>
2016-11-07 14:19:03 -0600

    #281 Params were added to get checkin photo data

--- ios_app/barista/PhotoCheckin.swift
+++ ios_app/barista/PhotoCheckin.swift
@@ -9,9 +9,17 @@
 import Foundation
 
 class PhotoCheckin {
-    var id: String
+    var id: Int
+    var fileName: String
+    var urlFile: String
+    var createdAt: String
+    var updatedAt: String
     
-    init(id: String) {
+    init(id: Int, fileName: String, urlFile: String, createdAt: String, updatedAt: String) {
         self.id = id
+        self.fileName = fileName
+        self.urlFile = urlFile
+        self .createdAt = createdAt
+        self.updatedAt = updatedAt
     }
 }
