279a7a025422f2ecafc185ec12d7b98e960d3e21
Ariana Santillán <arianarsdev@gmail.com>
2016-11-07 14:39:57 -0600

    #281 JSON Response was parsed and saved in PhotoCheckin

--- ios_app/barista/S3AssetManager.swift
+++ ios_app/barista/S3AssetManager.swift
@@ -8,20 +8,24 @@
 
 import Foundation
 import Alamofire
+import SwiftyJSON
 
 class S3AssetManager {
     
-    static func upload(uploadCommand: UploadCommand, onPhotoSuccess: @escaping (_ photoCheckin: PhotoCheckin) -> (), onPhotoError: @escaping (_ error: String) -> () ) {
+    static func uploadCheckinPhoto(uploadCommand: UploadCommand, onPhotoSuccess: @escaping (_ photoCheckin: PhotoCheckin) -> (), onPhotoError: @escaping (_ error: String) -> () ) {
         
-        let assetURL = try! URLRequest(url: URL(string: "http://localhost:3000/checkin/photo/save")!, method: .post, headers: nil)
-        let image: Data = UIImageJPEGRepresentation(uploadCommand.image, 0.5)!
+        let assetURL = try! URLRequest(url: URL(string: "http://localhost:3000/barista/photo/save_photo")!, method: .post, headers: nil)
+        let image: Data = UIImageJPEGRepresentation(uploadCommand.image, 1)!
+        let imageName: String = "file"
+        let imageFileName: String = "checkin.png"
+        let imageMimeType: String = "image/png"
         
         Alamofire.upload(
             multipartFormData: { multipartFromData in
                 multipartFromData.append(image,
-                    withName: uploadCommand.imageName,
-                    fileName: uploadCommand.imageFileName,
-                    mimeType: uploadCommand.imageMimeType
+                    withName: imageName,
+                    fileName: imageFileName,
+                    mimeType: imageMimeType
                 )
             },
             with: assetURL,
@@ -30,13 +34,20 @@ class S3AssetManager {
                 case .success(let upload, _, _):
                     upload.responseJSON {
                         response in
-                        print(response)
+                        if let value = response.result.value {
+                            let json = JSON(value)
+                            let s3Id = json["id"].intValue
+                            let s3FileName = json["name_file"].stringValue
+                            let s3UrlFile = json["url_file"].stringValue
+                            let s3CreatedAt = json["created_at"].stringValue
+                            let s3UpdatedAt = json["updated_at"].stringValue
+                            let s3Asset = PhotoCheckin(id: s3Id, fileName: s3FileName, urlFile: s3UrlFile, createdAt: s3CreatedAt, updatedAt: s3UpdatedAt)
+                                onPhotoSuccess(s3Asset)
+                        }
                     }
                 case .failure(let encodingError):
                     print(encodingError)
                 }
         })
     }
-    
-    
 }
