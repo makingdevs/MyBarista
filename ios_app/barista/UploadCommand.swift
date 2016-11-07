ba026654633c7177d22f4ebb3bd194144f053dd1
Ariana Santillán <arianarsdev@gmail.com>
2016-11-07 14:32:30 -0600

    #281 Unnecessary params were removed

--- ios_app/barista/UploadCommand.swift
+++ ios_app/barista/UploadCommand.swift
@@ -10,22 +10,10 @@ import Foundation
 import UIKit
 
 class UploadCommand {
-    
-    var userId: String
-    var checkinId: String
+
     var image: UIImage
-    var imagePath: String
-    var imageName: String
-    var imageFileName: String
-    var imageMimeType: String
     
-    init(userId: String, checkinId: String, image: UIImage, imagePath: String, imageName: String, imageFileName: String, imageMimeType: String) {
-        self.userId = userId
-        self.checkinId = checkinId
+    init(image: UIImage) {
         self.image = image
-        self.imagePath = imagePath
-        self.imageName = imageName
-        self.imageFileName = imageFileName
-        self.imageMimeType = imageMimeType
     }
 }
