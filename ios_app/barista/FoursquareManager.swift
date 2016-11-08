0c1e4c555fceda6e4952d6f8a7fe38997dab31fd
Ariana Santillán <arianarsdev@gmail.com>
2016-11-08 17:57:52 -0600

    #282 HTTP GET request to fetch near venues was realized

--- ios_app/barista/FoursquareManager.swift
+++ ios_app/barista/FoursquareManager.swift
@@ -7,10 +7,26 @@
 //
 
 import Foundation
+import Alamofire
 
 class FoursquareManager {
     
     static func getVenuesNear(venueCommand: VenueCommand, onSuccess: @escaping (_ venue: Venue) -> (), onError: @escaping (_ error: String) -> () ) {
-        // Our code here
+        let venuesURL: String = "http://localhost:3000/foursquare/searh_venues"
+        let parameters = [ "latitude": venueCommand.latitude,
+                           "longitude": venueCommand.longitude,
+                           "query": venueCommand.query]
+        
+        Alamofire.request(venuesURL, parameters: parameters)
+            .validate(statusCode: 200..<202)
+            .responseJSON {
+                response in
+                switch response.result {
+                case .success:
+                    print(response)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+        }
     }
 }
