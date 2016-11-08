33dff6383c33e01c9deffdf623dac16372513e04
Ariana Santillán <arianarsdev@gmail.com>
2016-11-08 16:59:25 -0600

    #282 Function to get near venues was created in FoursquareManager

---
+++ ios_app/barista/FoursquareManager.swift
@@ -0,0 +1,16 @@
+//
+//  FoursquareManager.swift
+//  barista
+//
+//  Created by Ariana Santillán on 08/11/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import Foundation
+
+class FoursquareManager {
+    
+    static func getVenuesNear(venueCommand: VenueCommand, onSuccess: @escaping (_ venue: Venue) -> (), onError: @escaping (_ error: String) -> () ) {
+        // Our code here
+    }
+}
