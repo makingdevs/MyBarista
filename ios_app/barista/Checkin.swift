fc6b75d9f6c83fb374d05f6671f2a2d263327303
Ariana Santillán <arianarsdev@gmail.com>
2016-11-14 09:59:25 -0600

    #289 Initializer was added to use it in case s3asset exists in check-in JSON response

--- ios_app/barista/Checkin.swift
+++ ios_app/barista/Checkin.swift
@@ -35,6 +35,20 @@ class Checkin {
         self.createdAt = createdAt
     }
     
+    /* s3asset exists in JSON response */
+    init(id:NSInteger, author:String, method:String, note:String, origin:String, state: String, price:String, rating: Float, venue: String?, createdAt:Date?) {
+        self.id = id
+        self.author = author
+        self.method = method
+        self.note = note
+        self.origin = origin
+        self.state = state
+        self.price = price
+        self.rating = rating
+        self.venue = venue
+        self.createdAt = createdAt
+    }
+    
     init(id: Int, note: String) {
         self.id = id
         self.note = note
