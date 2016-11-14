1b0104ce7f644a6a53acaa8fa55fdc5207a68359
Ariana Santillán <arianarsdev@gmail.com>
2016-11-14 16:03:33 -0600

    #294 Flavor params were added to CircleFlavor model

--- ios_app/barista/CircleFlavor.swift
+++ ios_app/barista/CircleFlavor.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+
+class CircleFlavor {
+    var sweetness: Int?
+    var acidity: Int?
+    var flowery: Int?
+    var spicy: Int?
+    var salty: Int?
+    var berries: Int?
+    var chocolate: Int?
+    var candy: Int?
+    var body: Int?
+    var cleaning: Int?
+    
+    init(sweetness: Int?, acidity: Int?, flowery: Int?, spicy: Int?, salty: Int?, berries: Int?, chocolate: Int?, candy: Int?, body: Int?, cleaning: Int?) {
+        self. sweetness = sweetness
+        self.acidity = acidity
+        self.flowery = flowery
+        self.spicy = spicy
+        self.salty = salty
+        self.berries = berries
+        self.chocolate = chocolate
+        self.candy = candy
+        self.body = body
+        self.cleaning = cleaning
+    }
+}
