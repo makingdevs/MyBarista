b1408019a679412f2d0ba3d77fa5b06b63f772d3
Ariana Santillán <arianarsdev@gmail.com>
2016-11-15 09:28:43 -0600

    #294 CreateCircleFlavor controller was added to the project

---
+++ ios_app/barista/CreateCircleFlavorViewController.swift
@@ -0,0 +1,17 @@
+//
+//  CreateCircleFlavorViewController.swift
+//  barista
+//
+//  Created by MakingDevs on 15/11/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class CreateCircleFlavorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Create circle flavor")
+    }
+}
