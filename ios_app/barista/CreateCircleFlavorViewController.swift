80c728d608bfbafde7acbcbece329166839f36a0
Ariana Santillán <arianarsdev@gmail.com>
2016-11-15 11:16:09 -0600

    #294 Circle flavor was created/updated successfully

--- ios_app/barista/CreateCircleFlavorViewController.swift
+++ ios_app/barista/CreateCircleFlavorViewController.swift
@@ -10,8 +10,35 @@ import UIKit
 
 class CreateCircleFlavorViewController: UIViewController {
 
+    var checkin: Checkin!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Create circle flavor")
+    }
+    
+    @IBAction func saveCircleFlavor(_ sender: UIButton) {
+        CheckinManager.createCircleFlavor(
+            checkinId: checkin.id,
+            circleFlavor:  getCircleFlavor(),
+            onSuccess: {(checkin: Checkin) -> () in
+                print(checkin.circleFlavor)
+            },
+            onError: {(error: String) -> () in
+                print(error)
+        })
+    }
+    
+    func getCircleFlavor() -> CircleFlavor {
+        let circleFlavor: CircleFlavor = CircleFlavor(sweetness: 1,
+                                                      acidity: 6,
+                                                      flowery: 1,
+                                                      spicy: 1,
+                                                      salty: 1,
+                                                      berries: 1,
+                                                      chocolate: 1,
+                                                      candy: 1,
+                                                      body: 1,
+                                                      cleaning: 1)
+        return circleFlavor
     }
 }
