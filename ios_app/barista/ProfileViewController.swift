b0dd6474f25ef7cf92a0b48cb2454427064369bc
Ariana Santillán <arianarsdev@gmail.com>
2016-11-17 16:41:28 -0600

    #299 ProfileViewController was added to the project

--- ios_app/barista/ProfileViewController.swift
+++ ios_app/barista/ProfileViewController.swift
@@ -2,7 +2,7 @@
 //  ProfileViewController.swift
 //  barista
 //
-//  Created by Ariana Santillán on 17/11/16.
+//  Created by MakingDevs on 17/11/16.
 //  Copyright © 2016 MakingDevs. All rights reserved.
 //
 
@@ -12,24 +12,5 @@ class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
