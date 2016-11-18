da156bd7a7c811137efb3d5cd42f6c6e987018c8
Ariana Santillán <arianarsdev@gmail.com>
2016-11-17 18:49:54 -0600

    #299 Function to make circle frames was added to UIImageView extensions

--- ios_app/barista/ImageViewExtensions.swift
+++ ios_app/barista/ImageViewExtensions.swift
@@ -17,4 +17,11 @@ extension UIImageView {
     func loadUrlWithPlaceholder(url: String, placeholder: UIImage) {
         self.kf.setImage(with: URL(string: url), placeholder: placeholder)
     }
+    
+    func circleFrame() {
+        self.layer.cornerRadius = self.frame.size.width / 2;
+        self.clipsToBounds = true
+        self.layer.borderWidth = 3
+        self.layer.borderColor = UIColor.white.cgColor
+    }
 }
