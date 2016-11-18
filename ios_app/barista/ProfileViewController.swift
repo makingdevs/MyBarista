d6b9ffd41fd55a0dbf92e739c4aa62b1af403d2b
Ariana Santillán <arianarsdev@gmail.com>
2016-11-17 18:50:54 -0600

    #299 User profile data was setted

--- ios_app/barista/ProfileViewController.swift
+++ ios_app/barista/ProfileViewController.swift
@@ -10,24 +10,43 @@ import UIKit
 
 class ProfileViewController: UIViewController {
     
+    @IBOutlet weak var blurAvatarImageView: UIImageView!
+    @IBOutlet weak var avatarImageView: UIImageView!
+    @IBOutlet weak var usernameLabel: UILabel!
+    @IBOutlet weak var coffeeCountLabel: UILabel!
+    @IBOutlet weak var webProfileLabel: UILabel!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var lastNameLabel: UILabel!
+    
     var username: String!
     var userId: Int!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         fetchUserPreferences()
-        showUserProfile(id: self.userId)
+        fetchUserProfile(id: self.userId)
     }
     
-    func showUserProfile(id: Int) {
+    func fetchUserProfile(id: Int) {
         UserManager.fetchProfile(
             userId: id,
             onSuccess: {(user: UserProfile) -> () in
-                // Something
+                self.showUserProfile(currentUser: user)
             },
             onError: {(error: String) -> () in
-                // Something
+                print(error)
         })
+    }
+    
+    func showUserProfile(currentUser: UserProfile) {
+        avatarImageView.circleFrame()
+        blurAvatarImageView.loadURL(url: (currentUser.s3asset?.urlFile)!)
+        avatarImageView.loadURL(url: (currentUser.s3asset?.urlFile)!)
+        usernameLabel.text = currentUser.username
+        coffeeCountLabel.text = String(describing: currentUser.checkinsCount!)
+        webProfileLabel.text = "http://users.barist.coffee/#profile/\(currentUser.username!)"
+        nameLabel.text = currentUser.name
+        lastNameLabel.text = currentUser.lastName
     }
     
     func fetchUserPreferences() {
