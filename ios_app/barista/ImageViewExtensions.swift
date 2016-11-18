fc88cca56dbfbd94aff8942a5ecc029634a8e9e8
Ariana Santillán <arianarsdev@gmail.com>
2016-11-18 10:41:45 -0600

    #299 Function to make blur images was added to UIImageView extensions

--- ios_app/barista/ImageViewExtensions.swift
+++ ios_app/barista/ImageViewExtensions.swift
@@ -14,14 +14,24 @@ extension UIImageView {
     func loadURL(url: String) {
         self.kf.setImage(with: URL(string: url))
     }
+    
+    func loadUrlWithBlur(url: String) {
+        let blur = UIBlurEffect(style: UIBlurEffectStyle.light)
+        let blurView = UIVisualEffectView(effect: blur)
+        self.kf.setImage(with: URL(string: url))
+        blurView.frame = self.bounds
+        self.addSubview(blurView)
+    }
+    
     func loadUrlWithPlaceholder(url: String, placeholder: UIImage) {
         self.kf.setImage(with: URL(string: url), placeholder: placeholder)
     }
     
-    func circleFrame() {
+    func loadAvatar(url: String) {
         self.layer.cornerRadius = self.frame.size.width / 2;
         self.clipsToBounds = true
         self.layer.borderWidth = 3
         self.layer.borderColor = UIColor.white.cgColor
+        self.kf.setImage(with: URL(string: url))
     }
 }
