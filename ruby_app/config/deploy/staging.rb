32f97cf79ee588effbad8c017a4e3ae67a6c9464
neodevelop <juan@makingdevs.com>
2016-11-18 11:20:57 -0600

    #300 put environments in place, adding dirs and redeploying

--- ruby_app/config/deploy/staging.rb
+++ ruby_app/config/deploy/staging.rb
@@ -2,7 +2,12 @@
 # ======================
 # Defines a single server with a list of roles and multiple properties.
 # You can define all roles on a single server, or split them:
+set :stage, :stage
+set :puma_bind, %w(tcp://0.0.0.0:3001)
+set :branch, 'stage'
+set :deploy_to, "/var/www/#{fetch(:application)}/stage"
 
+server 'mybarista.makingdevs.com', user: 'ec2-user', roles: %w{app web}
 # server 'example.com', user: 'deploy', roles: %w{app db web}, my_property: :my_value
 # server 'example.com', user: 'deploy', roles: %w{app web}, other_property: :other_value
 # server 'db.example.com', user: 'deploy', roles: %w{db}
