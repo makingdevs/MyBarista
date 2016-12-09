7bf10898e7a73f09e21c6b2de880b9d6ea95eed9
Ariana Santillán <arianarsdev@gmail.com>
2016-12-09 11:16:26 -0600

    #308 CommentManager was added to the project to fetch and save check-in comments

---
+++ ios_app/barista/CommentManager.swift
@@ -0,0 +1,15 @@
+//
+//  CommentManager.swift
+//  barista
+//
+//  Created by MakingDevs on 09/12/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import Foundation
+
+
+class CommentManager {
+    
+    static let sharedInstance = CommentManager()
+}
