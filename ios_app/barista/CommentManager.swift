9b23ba02851aeae2cc09c52e67db2c41ee332c55
Ariana Santillán <arianarsdev@gmail.com>
2016-12-09 11:40:26 -0600

    #308 Function to fetch check-in comments was created

--- ios_app/barista/CommentManager.swift
+++ ios_app/barista/CommentManager.swift
@@ -7,9 +7,26 @@
 //
 
 import Foundation
-
+import Alamofire
 
 class CommentManager {
     
     static let sharedInstance = CommentManager()
+    
+    func fetchComments(checkinId: Int, onSuccess: @escaping (_ comments: [Comment]) -> (), onError:@escaping (_ error: String) -> ()) {
+        
+        let commentsPath: String = "\(Constants.urlBase)/checkins/\(checkinId)/comments"
+        
+        Alamofire.request(commentsPath)
+            .validate(statusCode: 200..<202)
+            .responseJSON {
+                response in
+                switch response.result {
+                case .success:
+                    print(response)
+                case .failure(let error):
+                    print(error.localizedDescription)
+                }
+        }
+    }
 }
