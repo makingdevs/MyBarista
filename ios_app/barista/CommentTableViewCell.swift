672d586fc0a17196afdb9b1e3a74d52395c089b8
Ariana Gothwski <arianarsdev@gmail.com>
2017-01-02 14:42:15 -0600

    #308 Custom table view was created

---
+++ ios_app/barista/CommentTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CommentTableViewCell.swift
+//  barista
+//
+//  Created by Ariana Gothwski on 22/12/16.
+//  Copyright © 2016 MakingDevs. All rights reserved.
+//
+
+import UIKit
+
+class CommentTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+}
