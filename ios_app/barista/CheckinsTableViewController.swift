9e48dca30f7b52b53b81727defdacd99c79e7cf4
antonio <makingdevs.com>
2018-03-16 17:01:16 -0600

    #297 fixing created-at field presentation

--- ios_app/barista/CheckinsTableViewController.swift
+++ ios_app/barista/CheckinsTableViewController.swift
@@ -24,6 +24,19 @@ class CheckinsTableViewController: UITableViewController {
         }
     }
     
+    fileprivate func getDaysSinceNow(from someDate: Date) -> String {
+        let yesterday = Date(timeInterval: -86400, since: someDate)
+        let tomorrow = Date(timeInterval: 86400, since: Date())
+        let diff = tomorrow.interval(ofComponent: .day, fromDate: yesterday)
+        
+        if diff < 1{
+            return "Hace unas hroas"
+        }else if diff == 1 {
+            return "Hace un día"
+        }
+        return "Hace \(diff) dias"
+    }
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //TODO: Improve this piece of code
         let viewCell = tableView.dequeueReusableCell(withIdentifier: "CheckinsTableViewCell", for: indexPath) as! CheckinsTableViewCell
@@ -31,12 +44,14 @@ class CheckinsTableViewController: UITableViewController {
         viewCell.methodLabel?.text = checkin.method
         viewCell.coffeeOriginLabel?.text = checkin.state
         viewCell.ratingView.loadRating(rating: checkin.rating!)
-        viewCell.createdAtLabel.text = checkin.createdAt?.relativeTime
+        viewCell.createdAtLabel.text = self.getDaysSinceNow(from: checkin.createdAt!)
         if (checkin.s3Asset != nil) {
             viewCell.coffeeImageView.loadURL(url: (checkin.s3Asset?.urlFile)!)
         }
         return viewCell
     }
+    
+    
     
     override func numberOfSections(in tableView: UITableView) -> Int {
         return 1
@@ -66,3 +81,16 @@ class CheckinsTableViewController: UITableViewController {
     }
 }
 
+//TODO move it to a new file
+extension Date {
+    
+    func interval(ofComponent comp: Calendar.Component, fromDate date: Date) -> Int {
+        
+        let currentCalendar = Calendar.current
+        
+        guard let start = currentCalendar.ordinality(of: comp, in: .era, for: date) else { return 0 }
+        guard let end = currentCalendar.ordinality(of: comp, in: .era, for: self) else { return 0 }
+        
+        return end - start
+    }
+}
